ba9b63e9230dc2abdb4521961622016989bcfbfd
Stuart McCulloch <mcculls@gmail.com>
2020-01-27 00:35:37 -0800

    Public export of Nexus code:   - 5e3e0babc39029ef391d6b3ffa63e3db3e6da08f by Stuart McCulloch <mcculls@gmail.com>
    GitOrigin-RevId: 5e3e0babc39029ef391d6b3ffa63e3db3e6da08f

--- components/nexus-datastore-api/src/main/java/org/sonatype/nexus/datastore/api/IterableDataAccess.java
+++ components/nexus-datastore-api/src/main/java/org/sonatype/nexus/datastore/api/IterableDataAccess.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import org.sonatype.nexus.common.entity.EntityId;
 import org.sonatype.nexus.common.entity.HasEntityId;
+import org.sonatype.nexus.common.entity.HasName;
 
 /**
  * Generic CRUD {@link DataAccess} that accesses zero to many entities of the same type.
@@ -49,4 +50,23 @@ public interface IterableDataAccess<E extends HasEntityId>
    * Delete the entity with the given id.
    */
   boolean delete(EntityId id);
+
+  /**
+   * {@link IterableDataAccess} extension for entities that can also be accessed by name.
+   *
+   * @since 3.21
+   */
+  interface WithName<E extends HasName & HasEntityId>
+      extends IterableDataAccess<E>
+  {
+    /**
+     * Retrieve the entity with the given name.
+     */
+    Optional<E> readByName(String name);
+
+    /**
+     * Delete the entity with the given name.
+     */
+    boolean deleteByName(String name);
+  }
 }
