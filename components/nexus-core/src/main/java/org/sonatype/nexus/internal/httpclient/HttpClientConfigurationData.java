f3902468af6a798d4adc7327685295e83deb2e17
Stuart McCulloch <mcculls@gmail.com>
2020-02-02 10:00:00 +0000

    Public export of Nexus code:   - 5fcea6c2b92a90067488384a7c678ce269aef138 by Stuart McCulloch <mcculls@gmail.com>
    GitOrigin-RevId: 5fcea6c2b92a90067488384a7c678ce269aef138

---
+++ components/nexus-core/src/main/java/org/sonatype/nexus/internal/httpclient/HttpClientConfigurationData.java
@@ -0,0 +1,128 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.internal.httpclient;
+
+import javax.annotation.Nullable;
+import javax.validation.Valid;
+
+import org.sonatype.nexus.httpclient.config.AuthenticationConfiguration;
+import org.sonatype.nexus.httpclient.config.ConnectionConfiguration;
+import org.sonatype.nexus.httpclient.config.HttpClientConfiguration;
+import org.sonatype.nexus.httpclient.config.ProxyConfiguration;
+
+import org.apache.http.client.RedirectStrategy;
+
+/**
+ * {@link HttpClientConfiguration} data.
+ *
+ * @since 3.21
+ */
+public class HttpClientConfigurationData
+    implements HttpClientConfiguration, Cloneable
+{
+  @Valid
+  @Nullable
+  private ConnectionConfiguration connection;
+
+  @Valid
+  @Nullable
+  private ProxyConfiguration proxy;
+
+  @Valid
+  @Nullable
+  private RedirectStrategy redirectStrategy;
+
+  /**
+   * @see AuthenticationConfigurationDeserializer
+   */
+  @Valid
+  @Nullable
+  private AuthenticationConfiguration authentication;
+
+  @Override
+  @Nullable
+  public ConnectionConfiguration getConnection() {
+    return connection;
+  }
+
+  @Override
+  public void setConnection(@Nullable final ConnectionConfiguration connection) {
+    this.connection = connection;
+  }
+
+  @Override
+  @Nullable
+  public ProxyConfiguration getProxy() {
+    return proxy;
+  }
+
+  @Override
+  public void setProxy(@Nullable final ProxyConfiguration proxy) {
+    this.proxy = proxy;
+  }
+
+  @Override
+  @Nullable
+  public AuthenticationConfiguration getAuthentication() {
+    return authentication;
+  }
+
+  @Override
+  public void setAuthentication(@Nullable final AuthenticationConfiguration authentication) {
+    this.authentication = authentication;
+  }
+
+  @Override
+  @Nullable
+  public RedirectStrategy getRedirectStrategy() {
+    return redirectStrategy;
+  }
+
+  @Override
+  public void setRedirectStrategy(@Nullable final RedirectStrategy redirectStrategy) {
+    this.redirectStrategy = redirectStrategy;
+  }
+
+  @Override
+  public HttpClientConfigurationData copy() {
+    try {
+      HttpClientConfigurationData copy = (HttpClientConfigurationData) clone();
+      if (connection != null) {
+        copy.connection = connection.copy();
+      }
+      if (proxy != null) {
+        copy.proxy = proxy.copy();
+      }
+      if (authentication != null) {
+        copy.authentication = authentication.copy();
+      }
+      if (redirectStrategy != null) {
+        // no real cloning/copying needed, as we are allowed to use a singleton instance
+        copy.redirectStrategy = redirectStrategy;
+      }
+      return copy;
+    }
+    catch (CloneNotSupportedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName() + "{" +
+        "connection=" + connection +
+        ", proxy=" + proxy +
+        ", authentication=" + authentication +
+        '}';
+  }
+}
---
+++ components/nexus-core/src/main/java/org/sonatype/nexus/internal/httpclient/HttpClientConfigurationStoreImpl.java
@@ -0,0 +1,56 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.internal.httpclient;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.datastore.ConfigStoreSupport;
+import org.sonatype.nexus.datastore.api.DataSessionSupplier;
+import org.sonatype.nexus.httpclient.config.HttpClientConfiguration;
+import org.sonatype.nexus.transaction.Transactional;
+
+/**
+ * MyBatis {@link HttpClientConfigurationStore} implementation.
+ *
+ * @since 3.21
+ */
+@Named("mybatis")
+@Singleton
+public class HttpClientConfigurationStoreImpl
+    extends ConfigStoreSupport<HttpClientConfigurationDAO>
+    implements HttpClientConfigurationStore
+{
+  @Inject
+  public HttpClientConfigurationStoreImpl(final DataSessionSupplier sessionSupplier) {
+    super(sessionSupplier);
+  }
+
+  @Override
+  public HttpClientConfiguration newConfiguration() {
+    return new HttpClientConfigurationData();
+  }
+
+  @Transactional
+  @Override
+  public HttpClientConfiguration load() {
+    return dao().get().orElse(null);
+  }
+
+  @Transactional
+  @Override
+  public void save(final HttpClientConfiguration configuration) {
+    dao().set((HttpClientConfigurationData) configuration);
+  }
+}
