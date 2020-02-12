fe359543bcddeb8176e49ede37394184e7910902
Andrew Ivasenko <57669702+aivasenko@users.noreply.github.com>
2020-02-12 16:24:13 +0200

    Public export of Nexus code:   - b74974c380283b7160f4418db7e57809aa22d70e by Andrew Ivasenko <57669702+aivasenko@users.noreply.github.com>
    GitOrigin-RevId: b74974c380283b7160f4418db7e57809aa22d70e

---
+++ plugins/nexus-repository-pypi/src/main/java/org/sonatype/nexus/repository/pypi/rest/PypiProxyRepositoryApiRequest.java
@@ -0,0 +1,48 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.pypi.rest;
+
+import org.sonatype.nexus.repository.pypi.internal.PyPiFormat;
+import org.sonatype.nexus.repository.rest.api.model.CleanupPolicyAttributes;
+import org.sonatype.nexus.repository.rest.api.model.HttpClientAttributes;
+import org.sonatype.nexus.repository.rest.api.model.NegativeCacheAttributes;
+import org.sonatype.nexus.repository.rest.api.model.ProxyAttributes;
+import org.sonatype.nexus.repository.rest.api.model.ProxyRepositoryApiRequest;
+import org.sonatype.nexus.repository.rest.api.model.StorageAttributes;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @since 3.next
+ */
+@JsonIgnoreProperties({"format", "type"})
+public class PypiProxyRepositoryApiRequest
+    extends ProxyRepositoryApiRequest
+{
+  @JsonCreator
+  @SuppressWarnings("squid:S00107") // suppress constructor parameter count
+  public PypiProxyRepositoryApiRequest(
+      @JsonProperty("name") final String name,
+      @JsonProperty("online") final Boolean online,
+      @JsonProperty("storage") final StorageAttributes storage,
+      @JsonProperty("cleanup") final CleanupPolicyAttributes cleanup,
+      @JsonProperty("proxy") final ProxyAttributes proxy,
+      @JsonProperty("negativeCache") final NegativeCacheAttributes negativeCache,
+      @JsonProperty("httpClient") final HttpClientAttributes httpClient,
+      @JsonProperty("routingRule") final String routingRule)
+  {
+    super(name, PyPiFormat.NAME, online, storage, cleanup, proxy, negativeCache, httpClient, routingRule);
+  }
+}
