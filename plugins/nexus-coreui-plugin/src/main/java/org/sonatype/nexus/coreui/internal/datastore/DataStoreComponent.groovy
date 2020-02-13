3aec44cf2447e37f283c077ff87daf162b944a2e
Matthew Piggott <mpiggott@sonatype.com>
2020-02-12 23:54:17 -0500

    Public export of Nexus code:   - ba996135349b0030ce8b6e55b323e57907317ace by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: ba996135349b0030ce8b6e55b323e57907317ace

--- plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/internal/datastore/DataStoreComponent.groovy
+++ plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/internal/datastore/DataStoreComponent.groovy
@@ -10,36 +10,27 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.coreui
+package org.sonatype.nexus.coreui.internal.datastore
 
 import javax.annotation.Nullable
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
-import javax.validation.Valid
-import javax.validation.constraints.NotNull
-import javax.validation.groups.Default
 
 import org.sonatype.nexus.datastore.DataStoreConfigurationSource
 import org.sonatype.nexus.datastore.DataStoreDescriptor
 import org.sonatype.nexus.datastore.api.DataStore
-import org.sonatype.nexus.datastore.api.DataStoreConfiguration
 import org.sonatype.nexus.datastore.api.DataStoreManager
 import org.sonatype.nexus.extdirect.DirectComponentSupport
 import org.sonatype.nexus.rapture.StateContributor
 import org.sonatype.nexus.repository.manager.RepositoryManager
 import org.sonatype.nexus.repository.security.RepositoryPermissionChecker
 import org.sonatype.nexus.security.privilege.ApplicationPermission
-import org.sonatype.nexus.validation.Validate
-import org.sonatype.nexus.validation.group.Create
-import org.sonatype.nexus.validation.group.Update
 
 import com.codahale.metrics.annotation.ExceptionMetered
 import com.codahale.metrics.annotation.Timed
 import com.softwarementors.extjs.djn.config.annotations.DirectAction
 import com.softwarementors.extjs.djn.config.annotations.DirectMethod
-import org.apache.shiro.authz.annotation.RequiresPermissions
-import org.hibernate.validator.constraints.NotEmpty
 
 import static java.util.Collections.singletonList
 import static org.sonatype.nexus.security.BreadActions.READ
@@ -111,81 +102,11 @@ class DataStoreComponent
     dataStores.collect { asDataStoreXO(it) }
   }
 
-  @DirectMethod
-  @Timed
-  @ExceptionMetered
-  @RequiresPermissions('nexus:datastores:read')
-  List<DataStoreTypeXO> readTypes() {
-    dataStoreDescriptors.collect { key, descriptor ->
-      new DataStoreTypeXO(
-          id: key,
-          name: descriptor.name,
-          formFields: descriptor.formFields.collect { FormFieldXO.create(it) },
-          isEnabled: descriptor.isEnabled()
-      )
-    }
-  }
-
-  @DirectMethod
-  @Timed
-  @ExceptionMetered
-  @RequiresPermissions('nexus:datastores:read')
-  List<DataStoreSourceXO> readSources() {
-    dataStoreConfigurationSources.collect { key, source ->
-      new DataStoreSourceXO(
-          id: key,
-          name: source.name,
-          isModifiable: source.isModifiable(),
-          isEnabled: source.isEnabled()
-      )
-    }
-  }
-
-  @DirectMethod
-  @Timed
-  @ExceptionMetered
-  @RequiresPermissions('nexus:datastores:create')
-  @Validate(groups = [Create, Default])
-  DataStoreXO create(final @NotNull @Valid DataStoreXO dataStore) {
-    return asDataStoreXO(dataStoreManager.create(asConfiguration(dataStore)))
-  }
-
-  @DirectMethod
-  @Timed
-  @ExceptionMetered
-  @RequiresPermissions('nexus:datastores:update')
-  @Validate(groups = [Update, Default])
-  DataStoreXO update(final @NotNull @Valid DataStoreXO dataStore) {
-    return asDataStoreXO(dataStoreManager.update(asConfiguration(dataStore)))
-  }
-
-  @DirectMethod
-  @Timed
-  @ExceptionMetered
-  @RequiresPermissions('nexus:datastores:delete')
-  @Validate
-  void remove(final @NotEmpty String name) {
-    dataStoreManager.delete(name)
-  }
-
-  static DataStoreConfiguration asConfiguration(final DataStoreXO dataStoreXO) {
-    new DataStoreConfiguration(
-        name: dataStoreXO.name,
-        type: dataStoreXO.type,
-        source: dataStoreXO.source,
-        attributes: dataStoreXO.attributes
-    )
-  }
-
   DataStoreXO asDataStoreXO(final DataStore dataStore) {
     String storeName = dataStore.configuration.name
     boolean isContentStore = dataStoreManager.isContentStore(storeName)
     def dataStoreXO = new DataStoreXO(
         name: storeName,
-        type: dataStore.configuration.type,
-        source: dataStore.configuration.source,
-        attributes: dataStore.configuration.attributes,
-        inUse: !isContentStore || repositoryManager.isDataStoreUsed(storeName),
         isContentStore: isContentStore
     )
     return dataStoreXO
---
+++ plugins/nexus-coreui-plugin/src/test/java/org/sonatype/nexus/coreui/internal/datastore/DataStoreComponentTest.groovy
@@ -0,0 +1,89 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.coreui.internal.datastore
+
+import org.sonatype.nexus.datastore.api.DataStore
+import org.sonatype.nexus.datastore.api.DataStoreConfiguration
+import org.sonatype.nexus.datastore.api.DataStoreManager
+import org.sonatype.nexus.repository.manager.RepositoryManager
+import org.sonatype.nexus.repository.security.RepositoryPermissionChecker
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * Test for {@link DataStoreComponent}
+ */
+class DataStoreComponentTest
+    extends Specification
+{
+
+  DataStoreManager dataStoreManager = Mock() {
+    isContentStore(_) >> { callRealMethod() } // default method
+  }
+
+  RepositoryManager repositoryManager = Mock()
+
+  RepositoryPermissionChecker repositoryPermissionChecker = Mock()
+
+  @Subject
+  DataStoreComponent dataStoreComponent = new DataStoreComponent(
+    dataStoreManager: dataStoreManager,
+    repositoryManager: repositoryManager,
+    repositoryPermissionChecker: repositoryPermissionChecker)
+
+    def 'Reading databases'() {
+      given:
+        DataStoreConfiguration contentConfig = new DataStoreConfiguration(name: 'content', type: 'jdbc',
+          source: 'local', attributes: [jdbcUrl: 'jdbc:h2:some/datastore/url'])
+        DataStoreConfiguration configConfig = new DataStoreConfiguration(name: 'config', type: 'jdbc',
+          source: 'local', attributes: [jdbcUrl: 'jdbc:postgresql:some/datastore/url'])
+        DataStore contentDataStore = Mock()
+        DataStore configDataStore = Mock()
+  
+      when: 'Browsing data stores'
+        def results = dataStoreComponent.read()
+  
+      then: 'The known datastores are returned'
+        _ * contentDataStore.configuration >> contentConfig
+        _ * configDataStore.configuration >> configConfig
+        1 * dataStoreManager.isContentStore('content') >> true
+        1 * dataStoreManager.isContentStore('config') >> false
+        1 * dataStoreManager.browse() >> [contentDataStore, configDataStore]
+        results.size() == 2
+        results[0].name == 'content'
+        results[0].isContentStore
+        results[1].name == 'config'
+        !results[1].isContentStore
+    }
+
+  def 'Reading H2 databases'() {
+    given:
+      DataStoreConfiguration usedConfig = new DataStoreConfiguration(name: 'valid', type: 'jdbc',
+        source: 'local', attributes: [jdbcUrl: 'jdbc:h2:some/datastore/url'])
+      DataStoreConfiguration unusedConfig = new DataStoreConfiguration(name: 'invalid', type: 'jdbc',
+        source: 'local', attributes: [jdbcUrl: 'jdbc:postgresql:some/datastore/url'])
+      DataStore usedDataStore = Mock()
+      DataStore unusedDataStore = Mock()
+
+    when: 'Browsing data stores'
+      def results = dataStoreComponent.readH2()
+
+    then: 'Only the datastore with h2 is returned'
+      _ * usedDataStore.configuration >> usedConfig
+      _ * unusedDataStore.configuration >> unusedConfig
+      1 * dataStoreManager.browse() >> [usedDataStore, unusedDataStore]
+      results.size() == 1
+      results[0].name == 'valid'
+  }
+}
