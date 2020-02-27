7611cc12101384aadf3f7f5b136b90c82c2c5891
Matthew Piggott <mpiggott@sonatype.com>
2020-02-27 15:12:04 -0500

    Public export of Nexus code:   - 20cb4e9c6b0724f7c8a50b8af7a82533981e9ddf by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: 20cb4e9c6b0724f7c8a50b8af7a82533981e9ddf

--- components/nexus-repository/src/main/java/org/sonatype/nexus/repository/browse/internal/orient/package-info.java
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/browse/internal/orient/package-info.java
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-@FeatureFlag(name = "nexus.orient.store.config")
+@FeatureFlag(name = "nexus.orient.store.content")
 package org.sonatype.nexus.repository.browse.internal.orient;
 
 import org.sonatype.nexus.common.app.FeatureFlag;
