b62039f30b74f90aac8a285ea484c6ec6d7d1dee
Stuart McCulloch <mcculls@gmail.com>
2020-04-09 14:01:54 +0100

    Public export of Nexus code:   - 762a9cd00f351b60bffc37fc86b8aef4d58509d0 by Stuart McCulloch <mcculls@gmail.com>
    GitOrigin-RevId: 762a9cd00f351b60bffc37fc86b8aef4d58509d0

---
+++ plugins/nexus-example-content/src/main/resources/static/rapture/NX/example/view/repository/recipe/ExampleHosted.js
@@ -0,0 +1,43 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2018-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+/*global Ext, NX*/
+
+/**
+ * Repository "Settings" form for example hosted content.
+ *
+ * @since 3.next
+ */
+Ext.define('NX.example.view.repository.recipe.ExampleHosted', {
+  extend: 'NX.coreui.view.repository.RepositorySettingsForm',
+  alias: 'widget.nx-coreui-repository-example-hosted',
+  requires: [
+    'NX.coreui.view.repository.facet.StorageFacet',
+    'NX.coreui.view.repository.facet.StorageFacetHosted',
+    'NX.coreui.view.repository.facet.CleanupPolicyFacet'
+  ],
+
+  /**
+   * @override
+   */
+  initComponent: function() {
+    var me = this;
+
+    me.items = [
+      {xtype: 'nx-coreui-repository-storage-facet'},
+      {xtype: 'nx-coreui-repository-storage-hosted-facet'},
+      {xtype: 'nx-coreui-repository-cleanup-policy-facet'}
+    ];
+
+    me.callParent();
+  }
+});
