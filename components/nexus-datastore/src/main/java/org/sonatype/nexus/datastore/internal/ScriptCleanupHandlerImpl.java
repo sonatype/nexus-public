459ad7c928f8cd36085aefcd322ad1fe4c231e3b
Brad Beck <bradley.beck@gmail.com>
2020-04-14 10:04:56 -0500

    Public export of Nexus code:   - fd8e4c720825fd7bf7b7de4d3612fa694363c8a5 by Brad Beck <bradley.beck@gmail.com>
    GitOrigin-RevId: fd8e4c720825fd7bf7b7de4d3612fa694363c8a5

--- components/nexus-datastore/src/main/java/org/sonatype/nexus/datastore/internal/ScriptCleanupHandlerImpl.java
+++ components/nexus-datastore/src/main/java/org/sonatype/nexus/datastore/internal/ScriptCleanupHandlerImpl.java
@@ -10,37 +10,27 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.orient.transaction;
+package org.sonatype.nexus.datastore.internal;
 
-import javax.annotation.Priority;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.goodies.common.ComponentSupport;
 import org.sonatype.nexus.common.script.ScriptCleanupHandler;
 
-import com.orientechnologies.orient.core.db.ODatabase.STATUS;
-import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
-import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
-
 /**
- * Orient handler that cleans up open transactions
+ * Stub DataStore implementation of {@link ScriptCleanupHandler}.
  *
- * @since 3.17
+ * @since 3.next
  */
-@Named("orient")
-@Priority(Integer.MAX_VALUE)
+@Named("datastore")
 @Singleton
-public class OrientScriptCleanupHandler
+public class ScriptCleanupHandlerImpl
     extends ComponentSupport
     implements ScriptCleanupHandler
 {
   @Override
   public void cleanup(final String context) {
-    ODatabaseDocument database = ODatabaseRecordThreadLocal.instance().getIfDefined();
-    if (database != null && database.getStatus() == STATUS.OPEN) {
-      database.close();
-      log.warn("{} left a database connection open. Any opened connections should also be explicitly closed.", context);
-    }
+    log.debug("Currently does nothing");
   }
 }
