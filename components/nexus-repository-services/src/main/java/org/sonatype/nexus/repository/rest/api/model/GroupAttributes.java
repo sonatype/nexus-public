e2b8b1991381d96831ce3d65dedd8143dd7bf952
Brad Beck <bradley.beck@gmail.com>
2020-05-13 11:59:06 -0500

    Public export of Nexus code:   - d8eb89cc9f5e7bbc2e3ad4e9f1e6d3c051cbe134 by Brad Beck <bradley.beck@gmail.com>
    GitOrigin-RevId: d8eb89cc9f5e7bbc2e3ad4e9f1e6d3c051cbe134

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/model/GroupAttributes.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/rest/api/model/GroupAttributes.java
@@ -14,10 +14,11 @@ package org.sonatype.nexus.repository.rest.api.model;
 
 import java.util.Collection;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
-import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * REST API model used to describe a group repository.
@@ -26,7 +27,7 @@ import org.hibernate.validator.constraints.NotEmpty;
  */
 public class GroupAttributes
 {
-  @ApiModelProperty(value = "Member repositories' names", dataType = "java.util.List", example = "maven-public")
+  @ApiModelProperty(value = "Member repositories' names", dataType = "[Ljava.lang.String;")
   @NotEmpty
   protected final Collection<String> memberNames;
 
