5534903dc66270aefbbde249f406d488e6833e88
thorin <117224+thorin@users.noreply.github.com>
2020-05-17 19:44:06 +0100

    NEXUS-23865 maven-metadata.xml forceRebuild flag not removed on rebuild
    Fixes issue https://issues.sonatype.org/browse/NEXUS-23865
    
    This issue can cause timeout errors when multiple parallel builds request one or more metadata files, all marked with the forceRebuildFlag.

--- plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/internal/orient/MavenFacetImpl.java
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/internal/orient/MavenFacetImpl.java
@@ -253,6 +253,8 @@ public class MavenFacetImpl
     final StorageTx tx = UnitOfWork.currentTx();
 
     removeRebuildFlag(asset);
+    tx.saveAsset(asset);
+
     Blob metadataBlob = tx.requireBlob(asset.requireBlobRef());
     Metadata metadata = MavenModels.readMetadata(metadataBlob.getInputStream());
 
