c42b41ef10752851399933f72ff9da958fcf92f2
Olu Shiyanbade <oshiyanbade@sonatype.com>
2020-06-30 15:00:56 +0100

    Public export of Nexus code:   - 61e78233dca32cf9fd27c7b56651d0123df6f423 by Olu Shiyanbade <oshiyanbade@sonatype.com>
    GitOrigin-RevId: 61e78233dca32cf9fd27c7b56651d0123df6f423

--- plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/content/maven/internal/recipe/MavenContentFacetImpl.java
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/content/maven/internal/recipe/MavenContentFacetImpl.java
@@ -13,6 +13,7 @@
 package org.sonatype.nexus.content.maven.internal.recipe;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -20,8 +21,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 
+import org.sonatype.nexus.common.event.EventManager;
 import org.sonatype.nexus.common.hash.HashAlgorithm;
 import org.sonatype.nexus.content.maven.MavenContentFacet;
+import org.sonatype.nexus.content.maven.internal.event.RebuildMavenArchetypeCatalogEvent;
 import org.sonatype.nexus.repository.config.Configuration;
 import org.sonatype.nexus.repository.config.ConfigurationFacet;
 import org.sonatype.nexus.repository.content.Component;
@@ -44,7 +47,7 @@ import org.sonatype.nexus.repository.types.ProxyType;
 import org.sonatype.nexus.repository.view.Payload;
 import org.sonatype.nexus.repository.view.payloads.TempBlob;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
 import org.apache.maven.model.Model;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -53,7 +56,9 @@ import static org.sonatype.nexus.common.hash.HashAlgorithm.SHA1;
 import static org.sonatype.nexus.content.maven.internal.recipe.AttributesHelper.assetKind;
 import static org.sonatype.nexus.content.maven.internal.recipe.AttributesHelper.getPackaging;
 import static org.sonatype.nexus.content.maven.internal.recipe.AttributesHelper.setAssetAttributes;
+import static org.sonatype.nexus.content.maven.internal.recipe.AttributesHelper.setComponentAttributes;
 import static org.sonatype.nexus.content.maven.internal.recipe.AttributesHelper.setPomAttributes;
+import static org.sonatype.nexus.content.maven.internal.recipe.MavenArchetypeCatalogFacetImpl.MAVEN_ARCHETYPE_KIND;
 import static org.sonatype.nexus.repository.maven.internal.Constants.METADATA_FILENAME;
 import static org.sonatype.nexus.repository.maven.internal.MavenModels.readModel;
 
@@ -69,11 +74,13 @@ public class MavenContentFacetImpl
 {
   private static final String CONFIG_KEY = "maven";
 
-  protected static final Map<HashAlgorithm, HashAlgorithm> HASHING = ImmutableMap.of(SHA1, SHA1, MD5, MD5);
+  protected static final List<HashAlgorithm> HASHING = ImmutableList.of(SHA1, MD5);
 
   private final Map<String, MavenPathParser> mavenPathParsers;
 
   private final MavenMetadataContentValidator metadataValidator;
+
+  private final EventManager eventManager;
 
   private final boolean metadataValidationEnabled;
 
@@ -103,11 +110,13 @@ public class MavenContentFacetImpl
       @Named(Maven2Format.NAME) final FormatStoreManager formatStoreManager,
       final Map<String, MavenPathParser> mavenPathParsers,
       final MavenMetadataContentValidator metadataValidator,
+      final EventManager eventManager,
       @Named("${nexus.maven.metadata.validation.enabled:-true}") final boolean metadataValidationEnabled)
   {
     super(formatStoreManager);
     this.mavenPathParsers = checkNotNull(mavenPathParsers);
     this.metadataValidator = metadataValidator;
+    this.eventManager = eventManager;
     this.metadataValidationEnabled = metadataValidationEnabled;
   }
 
@@ -115,9 +124,7 @@ public class MavenContentFacetImpl
   public Optional<Payload> get(final String path) {
     log.debug("GET {} : {}", getRepository().getName(), path);
 
-    return assets()
-        .path(path)
-        .find()
+    return findAsset(path)
         .map(FluentAsset::download);
   }
 
@@ -125,12 +132,18 @@ public class MavenContentFacetImpl
   public Payload put(final MavenPath path, final Payload content) throws IOException {
     log.debug("PUT {} : {}", getRepository().getName(), path);
 
-    try (TempBlob blob = blobs().ingest(content, HASHING.values())) {
+    try (TempBlob blob = blobs().ingest(content, HASHING)) {
       if (isMetadataAndValidationEnabled(path)) {
         validate(path, blob);
       }
       return save(path, blob);
     }
+  }
+
+  private Optional<FluentAsset> findAsset(final String path) {
+    return assets()
+        .path(path)
+        .find();
   }
 
   private boolean isMetadataAndValidationEnabled(final MavenPath path) {
@@ -161,9 +174,13 @@ public class MavenContentFacetImpl
         .version(coordinates.getVersion())
         .getOrCreate();
     if (isNewRepositoryContent(component)) {
-      AttributesHelper.setComponentAttributes(component, coordinates);
+      setComponentAttributes(component, coordinates);
     }
     return component;
+  }
+
+  private boolean isNewRepositoryContent(RepositoryContent repositoryContent) {
+    return repositoryContent.attributes().isEmpty();
   }
 
   private void maybeUpdateComponentAttributesFromModel(
@@ -174,6 +191,13 @@ public class MavenContentFacetImpl
     if (model != null) {
       component.kind(getPackaging(model));
       setPomAttributes(component, model);
+      publishEvents(component);
+    }
+  }
+
+  private void publishEvents(final FluentComponent component) {
+    if (MAVEN_ARCHETYPE_KIND.equals(component.kind())) {
+      eventManager.post(new RebuildMavenArchetypeCatalogEvent(getRepository().getName()));
     }
   }
 
@@ -198,15 +222,12 @@ public class MavenContentFacetImpl
     if (component != null) {
       assetBuilder = assetBuilder.component(component);
     }
+
     FluentAsset asset = assetBuilder.getOrCreate();
     if (isNewRepositoryContent(asset)) {
       setAssetAttributes(asset, path);
     }
     return asset.attach(blob).download();
-  }
-
-  private boolean isNewRepositoryContent(RepositoryContent repositoryContent) {
-    return repositoryContent.attributes().isEmpty();
   }
 
   @Override
@@ -219,11 +240,8 @@ public class MavenContentFacetImpl
     return assetIsDeleted;
   }
 
-  private Boolean deleteAsset(final MavenPath mavenPath) {
-    String path = mavenPath.getPath();
-    return assets()
-        .path(path)
-        .find()
+  private boolean deleteAsset(final MavenPath mavenPath) {
+    return findAsset(mavenPath.getPath())
         .map(FluentAsset::delete)
         .orElse(false);
   }
@@ -240,6 +258,7 @@ public class MavenContentFacetImpl
   private void deleteIfNoAssetsLeft(FluentComponent component) {
     if (component.assets().isEmpty()) {
       component.delete();
+      publishEvents(component);
     }
   }
 
