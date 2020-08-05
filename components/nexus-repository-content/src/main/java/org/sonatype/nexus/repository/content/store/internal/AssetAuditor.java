7b120213c61c90d8e37921082f5dda174b0c129c
Michael R Bucher <michaelrbucher@gmail.com>
2020-08-05 18:33:40 -0400

    Public export of Nexus code:   - d9834d8ad813142463ca6404edb98e99c6af9769 by Michael R Bucher <michaelrbucher@gmail.com>
    GitOrigin-RevId: d9834d8ad813142463ca6404edb98e99c6af9769

---
+++ components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/store/internal/AssetAuditor.java
@@ -0,0 +1,110 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.content.store.internal;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.audit.AuditData;
+import org.sonatype.nexus.audit.AuditorSupport;
+import org.sonatype.nexus.common.event.EventAware;
+import org.sonatype.nexus.repository.content.Asset;
+import org.sonatype.nexus.repository.content.event.asset.AssetAttributesEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetCreatedEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetDeletedEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetDownloadedEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetKindEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetPurgedEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetUpdatedEvent;
+import org.sonatype.nexus.repository.content.event.asset.AssetUploadedEvent;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+
+import static java.lang.String.format;
+
+/**
+ * Repository asset auditor.
+ *
+ * @since 3.next
+ */
+@Named
+@Singleton
+public class AssetAuditor
+    extends AuditorSupport
+    implements EventAware
+{
+  public static final String DOMAIN = "repository.asset";
+
+  public AssetAuditor() {
+    registerType(AssetCreatedEvent.class, CREATED_TYPE);
+
+    registerType(AssetDeletedEvent.class, DELETED_TYPE);
+
+    registerType(AssetPurgedEvent.class, PURGE_TYPE);
+
+    registerType(AssetUpdatedEvent.class, UPDATED_TYPE);
+    registerType(AssetAttributesEvent.class, UPDATED_TYPE + "-attribute");
+    registerType(AssetDownloadedEvent.class, UPDATED_TYPE + "-downloaded");
+    registerType(AssetKindEvent.class, UPDATED_TYPE + "-kind");
+    registerType(AssetUploadedEvent.class, UPDATED_TYPE + "-uploaded");
+  }
+
+  @Subscribe
+  @AllowConcurrentEvents
+  public void on(final AssetPurgedEvent event) {
+    if (isRecording()) {
+      AuditData data = new AuditData();
+      data.setDomain(DOMAIN);
+      data.setType(type(event.getClass()));
+      data.setContext(event.getRepository().getName());
+
+      Map<String, Object> attributes = data.getAttributes();
+      attributes.put("repository.name", event.getRepository().getName());
+      attributes.put("assetIds", Arrays.toString(event.getAssetIds()));
+
+      record(data);
+    }
+  }
+
+  @Subscribe
+  @AllowConcurrentEvents
+  public void on(final AssetEvent event) {
+    if (isRecording()) {
+      Asset asset = event.getAsset();
+
+      AuditData data = new AuditData();
+      data.setDomain(DOMAIN);
+      data.setType(type(event.getClass()));
+      data.setContext(asset.path());
+
+      Map<String, Object> attributes = data.getAttributes();
+      attributes.put("repository.name", event.getRepository().getName());
+      attributes.put("path", asset.path());
+      attributes.put("kind", asset.kind());
+
+      if (event instanceof AssetAttributesEvent){
+        AssetAttributesEvent attributesEvent = (AssetAttributesEvent) event;
+        attributes.put("attribute.change", attributesEvent.getChange());
+        attributes.put("attribute.key", attributesEvent.getKey());
+        attributes.put("attribute.value", attributesEvent.getValue());
+      }
+
+      record(data);
+    }
+  }
+}
