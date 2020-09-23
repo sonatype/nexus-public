60940def010e779922b9eb672023d8833f2e5674
Damian Bradicich <dbradicich@sonatype.com>
2020-09-23 14:41:23 -0400

    Public export of Nexus code:   - c8e7c5698cc8a62a8e472ea80b6357209dce07a9 by Damian Bradicich <dbradicich@sonatype.com>
    GitOrigin-RevId: c8e7c5698cc8a62a8e472ea80b6357209dce07a9

--- components/nexus-repository-config/src/main/java/org/sonatype/nexus/repository/internal/blobstore/BlobStoreManagerImpl.java
+++ components/nexus-repository-config/src/main/java/org/sonatype/nexus/repository/internal/blobstore/BlobStoreManagerImpl.java
@@ -412,11 +412,11 @@ public class BlobStoreManagerImpl
     destBlobStore.setBlobAttributes(blobId, srcBlobAttributes);
 
     ensureDeletedStateTransferred(blobId, srcBlobStore, destBlobStore, isSrcDelted);
-    log.info("Created blobId {} in blob store '{}'", blobId, destBlobStore.getBlobStoreConfiguration().getName());
+    log.debug("Created blobId {} in blob store '{}'", blobId, destBlobStore.getBlobStoreConfiguration().getName());
 
     try {
       srcBlobStore.deleteHard(blobId);
-      log.info("Removed blobId {} from blob store '{}'", blobId, srcBlobStore.getBlobStoreConfiguration().getName());
+      log.debug("Removed blobId {} from blob store '{}'", blobId, srcBlobStore.getBlobStoreConfiguration().getName());
     }
     catch (BlobStoreException e) {
       log.warn("Failed to remove blobId {} from blob store '{}'", blobId, srcBlobStore.getBlobStoreConfiguration().getName(), e);
