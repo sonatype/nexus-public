262cbaac3ee37a4f0afbde6af8d8c92c8b474460
Mark Dodgson <mdodgson@sonatype.com>
2020-10-07 09:11:57 +0100

    Public export of Nexus code:   - 4e8069a60da83b020f2c7c424ce463de5c63237d by Mark Dodgson <mdodgson@sonatype.com>
    GitOrigin-RevId: 4e8069a60da83b020f2c7c424ce463de5c63237d

---
+++ plugins/nexus-repository-npm/src/test/java/org/sonatype/nexus/repository/npm/internal/NpmAuditFacetTest.java
@@ -0,0 +1,114 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.npm.internal;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.cache.CacheHelper;
+import org.sonatype.nexus.common.event.EventManager;
+import org.sonatype.nexus.repository.npm.internal.audit.report.ReportCreator;
+import org.sonatype.nexus.repository.view.Context;
+import org.sonatype.nexus.repository.view.Headers;
+import org.sonatype.nexus.repository.view.Request;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+public class NpmAuditFacetTest
+    extends TestSupport
+{
+  private NpmAuditFacet underTest;
+
+  @Mock
+  private Context context;
+
+  @Mock
+  private Request request;
+
+  private Headers headers;
+
+  @Mock
+  private EventManager eventManager;
+
+  @Mock
+  private ReportCreator reportCreator;
+
+  @Mock
+  private CacheHelper cacheHelper;
+
+  @Before
+  public void setUp() throws Exception {
+
+    underTest = new NpmAuditFacet(1, 1, eventManager, reportCreator, cacheHelper);
+  }
+
+  @Test
+  public void whenNoHeadersAppIdNotFound() {
+    headers = new Headers();
+    when(context.getRequest()).thenReturn(request);
+    when(request.getHeaders()).thenReturn(headers);
+
+    final String actual = underTest.maybeGetAppId(context);
+
+    assertThat(actual, is(nullValue()));
+  }
+
+  @Test
+  public void whenAppIdIsPresentInValue() {
+    final ListMultimap multimap = ArrayListMultimap.create();
+    multimap.put("0", "app_id: bar");
+    headers = new Headers(multimap);
+    when(context.getRequest()).thenReturn(request);
+    when(request.getHeaders()).thenReturn(headers);
+
+    final String actual = underTest.maybeGetAppId(context);
+
+    assertThat(actual, is("bar"));
+  }
+
+  @Test
+  public void whenAppIdIsNotPresent() {
+    final ListMultimap multimap = ArrayListMultimap.create();
+    multimap.put("0", "appid: bar");
+    headers = new Headers(multimap);
+    when(context.getRequest()).thenReturn(request);
+    when(request.getHeaders()).thenReturn(headers);
+
+    final String actual = underTest.maybeGetAppId(context);
+
+    assertThat(actual, is(nullValue()));
+  }
+
+  @Test
+  public void whenAppIdIsPresentInList() {
+    final ListMultimap multimap = ArrayListMultimap.create();
+    multimap.put("Content-Type", "application/json");
+    multimap.put("0", "app_id: baz");
+    multimap.put("hello", "world");
+    headers = new Headers(multimap);
+    when(context.getRequest()).thenReturn(request);
+    when(request.getHeaders()).thenReturn(headers);
+
+    final String actual = underTest.maybeGetAppId(context);
+
+    assertThat(actual, is("baz"));
+  }
+}
