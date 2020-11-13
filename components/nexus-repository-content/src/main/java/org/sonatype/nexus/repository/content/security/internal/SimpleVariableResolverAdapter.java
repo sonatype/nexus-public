729f8227961c3b5ba2988268005304aa70f358d5
Brad Beck <bradley.beck@gmail.com>
2020-11-13 13:36:31 -0600

    Public export of Nexus code:   - 29d974fd1d4ad8e037df89bb930558d53da791e2 by Brad Beck <bradley.beck@gmail.com>
    GitOrigin-RevId: 29d974fd1d4ad8e037df89bb930558d53da791e2

--- components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/security/internal/SimpleVariableResolverAdapter.java
+++ components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/security/internal/SimpleVariableResolverAdapter.java
@@ -10,14 +10,15 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.repository.content.internal.security;
+package org.sonatype.nexus.repository.content.security.internal;
 
 import java.util.Map;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.sonatype.nexus.repository.security.VariableResolverAdapterSupport;
+import org.sonatype.nexus.repository.content.fluent.FluentAsset;
+import org.sonatype.nexus.repository.content.security.AssetVariableResolverSupport;
 import org.sonatype.nexus.repository.view.Request;
 import org.sonatype.nexus.selector.VariableSourceBuilder;
 
@@ -31,7 +32,7 @@ import org.elasticsearch.search.lookup.SourceLookup;
 @Named("simple")
 @Singleton
 public class SimpleVariableResolverAdapter
-    extends VariableResolverAdapterSupport
+    extends AssetVariableResolverSupport
 {
   @Override
   protected void addFromRequest(final VariableSourceBuilder builder, final Request request) {
@@ -43,6 +44,11 @@ public class SimpleVariableResolverAdapter
                                      final SourceLookup sourceLookup,
                                      final Map<String, Object> asset)
   {
+    // no-op the simple impl just allows for the path/format variable resolvers in the support class
+  }
+
+  @Override
+  protected void addFromAsset(final VariableSourceBuilder builder, final FluentAsset asset) {
     // no-op the simple impl just allows for the path/format variable resolvers in the support class
   }
 }
