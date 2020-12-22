b5e5c0878184c6524c14745e74f8e5a237650ffc
sonatype-zion <zion@sonatype.com>
2020-12-22 06:39:24 -0500

    upstream: b=master,r=a249eb622fd56a0f623fbb6d5f3d8a9c53680ac0,t=2020-12-22-0507-54369

---
+++ plugins/nexus-coreui-plugin/src/main/java/org/sonatype/nexus/coreui/internal/EarlyAccessStateContributor.java
@@ -0,0 +1,51 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.coreui.internal;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.common.app.FeatureFlags;
+import org.sonatype.nexus.rapture.StateContributor;
+
+import com.google.common.collect.ImmutableMap;
+
+@Singleton
+@Named
+public class EarlyAccessStateContributor
+    implements StateContributor
+{
+  private boolean earlyAccessDatastoreEnabled;
+
+  private boolean earlyAccessDatastoreDeveloper;
+
+  @Inject
+  public EarlyAccessStateContributor(
+      @Named("${nexus.earlyAccess.datastore.enabled:-false}") boolean earlyAccessDatastoreEnabled,
+      @Named("${nexus.earlyAccess.datastore.developer:-false}") boolean earlyAccessDatastoreDeveloper)
+  {
+    this.earlyAccessDatastoreEnabled = earlyAccessDatastoreEnabled;
+    this.earlyAccessDatastoreDeveloper = earlyAccessDatastoreDeveloper;
+  }
+
+  @Override
+  public Map<String, Object> getState() {
+    return ImmutableMap.of(
+        FeatureFlags.EARLY_ACCESS_DATASTORE, earlyAccessDatastoreEnabled,
+        FeatureFlags.EARLY_ACCESS_DATASTORE_DEVELOPER, earlyAccessDatastoreDeveloper
+    );
+  }
+}
