faab04da80500df5aa7cb03f6f723ac019d606ca
Brad Beck <bradley.beck@gmail.com>
2021-02-02 16:19:41 -0600

    Public export of Nexus code:   - 9f78236ed89da03159d12ca6aec3beeff6e695a2 by Brad Beck <bradley.beck@gmail.com>
    GitOrigin-RevId: 9f78236ed89da03159d12ca6aec3beeff6e695a2

--- plugins/nexus-repository-helm/src/main/java/org/sonatype/repository/helm/internal/orient/createindex/CreateIndexFacetImpl.java
+++ plugins/nexus-repository-helm/src/main/java/org/sonatype/repository/helm/internal/orient/createindex/CreateIndexFacetImpl.java
@@ -38,10 +38,10 @@ import org.sonatype.nexus.transaction.UnitOfWork;
 import org.sonatype.repository.helm.HelmAttributes;
 import org.sonatype.repository.helm.internal.AssetKind;
 import org.sonatype.repository.helm.internal.HelmFormat;
-import org.sonatype.repository.helm.internal.content.recipe.HelmHostedFacet;
 import org.sonatype.repository.helm.internal.createindex.CreateIndexFacet;
 import org.sonatype.repository.helm.internal.createindex.HelmIndexInvalidationEvent;
 import org.sonatype.repository.helm.internal.orient.HelmFacet;
+import org.sonatype.repository.helm.internal.orient.hosted.HelmHostedFacet;
 
 import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
