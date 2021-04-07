715907686aa4ace04b5eb531c4d8ee7191f473ac
sonatype-zion <zion@sonatype.com>
2021-04-07 09:12:57 -0400

    upstream: b=master,r=f09127f459e426d2badd608e66d6eefe32a5f6e6,t=2021-04-07-0735-18510

---
+++ plugins/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/internal/datastore/MavenFacetUtils.java
@@ -0,0 +1,38 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.maven.internal.datastore;
+
+import org.sonatype.nexus.repository.content.Component;
+import org.sonatype.nexus.repository.maven.internal.Maven2Format;
+
+import static org.sonatype.nexus.repository.maven.internal.Attributes.P_BASE_VERSION;
+import static org.sonatype.nexus.repository.maven.internal.Constants.SNAPSHOT_VERSION_SUFFIX;
+
+/**
+ * Maven facet utilities for datastore implementation.
+ *
+ * @since 3.next
+ */
+public final class MavenFacetUtils
+{
+  private MavenFacetUtils() {
+  }
+
+  /**
+   * Is a given Component a snapshot
+   */
+  public static boolean isSnapshot(final Component component) {
+    String baseVersion = (String) component.attributes().child(Maven2Format.NAME).get(P_BASE_VERSION);
+    return baseVersion != null && baseVersion.endsWith(SNAPSHOT_VERSION_SUFFIX);
+  }
+}
