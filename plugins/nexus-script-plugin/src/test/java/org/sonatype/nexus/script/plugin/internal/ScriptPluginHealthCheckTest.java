79552ef0ca25220dbe9704fc372332b3f7fbfa42
Santiago Del Valle <sdelvalle@sonatype.com>
2021-05-04 14:56:43 -0500

    Public export of Nexus code:   - 30703f8b6807a55f8051a364d29151f7265efb25 by Santiago Del Valle <sdelvalle@sonatype.com>
    GitOrigin-RevId: 30703f8b6807a55f8051a364d29151f7265efb25

---
+++ plugins/nexus-script-plugin/src/test/java/org/sonatype/nexus/script/plugin/internal/ScriptPluginHealthCheckTest.java
@@ -0,0 +1,52 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.script.plugin.internal;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.script.ScriptManager;
+
+import com.codahale.metrics.health.HealthCheck.Result;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+
+public class ScriptPluginHealthCheckTest extends TestSupport
+{
+  @Mock
+  ScriptManager scriptManager;
+
+  private ScriptPluginHealthCheck underTest;
+
+  @Before
+  public void setup() {
+    underTest = new ScriptPluginHealthCheck(scriptManager);
+  }
+
+  @Test
+  public void checkScriptManagerDisabled() {
+    when(scriptManager.isEnabled()).thenReturn(false);
+    Result result = underTest.check();
+    assertTrue(result.isHealthy());
+  }
+
+  @Test
+  public void checkScriptManagerEnabled() {
+    when(scriptManager.isEnabled()).thenReturn(true);
+    Result result = underTest.check();
+    assertFalse(result.isHealthy());
+  }
+}
