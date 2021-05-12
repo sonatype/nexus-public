b3987b5085a971f19ad0f602fba3a281b5d15bcd
sonatype-zion <zion@sonatype.com>
2021-05-12 06:14:38 -0400

    upstream: b=master,r=3fbaaf047ba0abcc0831bf445a32f57e00fc6762,t=2021-05-12-0435-2145

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/internal/browse/AptBrowseNodeGenerator.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/internal/browse/AptBrowseNodeGenerator.java
@@ -12,11 +12,24 @@
  */
 package org.sonatype.nexus.repository.apt.datastore.internal.browse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.repository.apt.internal.AptFormat;
+import org.sonatype.nexus.repository.browse.node.BrowsePath;
+import org.sonatype.nexus.repository.browse.node.BrowsePathBuilder;
+import org.sonatype.nexus.repository.content.Asset;
+import org.sonatype.nexus.repository.content.Component;
 import org.sonatype.nexus.repository.content.browse.DefaultBrowseNodeGenerator;
+
+import com.google.common.base.Splitter;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.repository.browse.node.BrowsePath.SLASH;
 
 /**
  * @since 3.next
@@ -26,4 +39,46 @@ import org.sonatype.nexus.repository.content.browse.DefaultBrowseNodeGenerator;
 public class AptBrowseNodeGenerator
     extends DefaultBrowseNodeGenerator
 {
+  private static final String PACKAGES_PATH = "packages";
+
+  private static final String METADATA_PATH = "metadata";
+
+  @Override
+  public List<BrowsePath> computeAssetPaths(final Asset asset) {
+    Component component = asset.component().orElse(null);
+    if (component != null) {
+      return computeComponentPaths(asset);
+    }
+
+    List<String> pathParts = new ArrayList<>();
+    String path = asset.path();
+    if (path.endsWith(".deb") || path.endsWith(".udeb")) {
+      pathParts.add(PACKAGES_PATH);
+    }
+    else if (!path.startsWith("/snapshots")) {
+      pathParts.add(METADATA_PATH);
+    }
+
+    pathParts.addAll(Splitter.on(SLASH).omitEmptyStrings().splitToList(path));
+
+    return BrowsePathBuilder.fromPaths(pathParts, true);
+  }
+
+  @Override
+  public List<BrowsePath> computeComponentPaths(final Asset asset) {
+    checkNotNull(asset);
+
+    checkArgument(asset.component().isPresent());
+    Component component = asset.component().get();
+
+    List<String> pathParts = new ArrayList<>();
+    pathParts.add(PACKAGES_PATH);
+    pathParts.add(component.name().substring(0, 1).toLowerCase());
+    pathParts.add(component.name());
+    pathParts.add(component.version());
+    pathParts.add(component.namespace());
+    pathParts.add(component.name());
+    pathParts.add(asset.path().substring(1));
+    return BrowsePathBuilder.fromPaths(pathParts, true);
+  }
 }
---
+++ plugins/nexus-repository-apt/src/test/java/org/sonatype/nexus/repository/apt/datastore/internal/AptBrowseNodeGeneratorTest.java
@@ -0,0 +1,109 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.apt.datastore.internal;
+
+import java.util.List;
+
+import org.sonatype.nexus.repository.apt.datastore.internal.browse.AptBrowseNodeGenerator;
+import org.sonatype.nexus.repository.browse.BrowseTestSupport;
+import org.sonatype.nexus.repository.browse.node.BrowsePath;
+import org.sonatype.nexus.repository.content.store.AssetData;
+import org.sonatype.nexus.repository.content.store.ComponentData;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @since 3.next
+ */
+public class AptBrowseNodeGeneratorTest
+    extends BrowseTestSupport
+{
+  private AptBrowseNodeGenerator generator = new AptBrowseNodeGenerator();
+
+  @Test
+  public void computeComponentPath() {
+    ComponentData componentData = new ComponentData();
+    componentData.setRepositoryId(1);
+    componentData.setComponentId(1);
+    componentData.setName("nano");
+    componentData.setNamespace("amd64");
+    componentData.setVersion("1.0.0");
+
+    AssetData asset = new AssetData();
+    asset.setComponent(componentData);
+    asset.setPath("/path/assetName");
+
+    List<BrowsePath> paths = generator.computeComponentPaths(asset);
+
+    assertThat(paths.size(), is(7));
+
+    assertThat(paths, containsInAnyOrder(
+        new BrowsePath("packages", "/packages/"),
+        new BrowsePath("n", "/packages/n/"),
+        new BrowsePath("nano", "/packages/n/nano/"),
+        new BrowsePath("1.0.0", "/packages/n/nano/1.0.0/"),
+        new BrowsePath("amd64", "/packages/n/nano/1.0.0/amd64/"),
+        new BrowsePath("nano", "/packages/n/nano/1.0.0/amd64/nano/"),
+        new BrowsePath("path/assetName", "/packages/n/nano/1.0.0/amd64/nano/path/assetName/")));
+  }
+
+
+  @Test
+  public void computeAssetPathMetadata() {
+    AssetData asset = new AssetData();
+    asset.setPath("/path/assetName");
+
+    List<BrowsePath> paths = generator.computeAssetPaths(asset);
+
+    assertThat(paths.size(), is(3));
+
+    assertThat(paths, containsInAnyOrder(
+        new BrowsePath("metadata", "/metadata/"),
+        new BrowsePath("path", "/metadata/path/"),
+        new BrowsePath("assetName", "/metadata/path/assetName/")));
+  }
+
+  @Test
+  public void computeAssetPathDeb() {
+    AssetData asset = new AssetData();
+    asset.setPath("/path/assetName.deb");
+
+    List<BrowsePath> paths = generator.computeAssetPaths(asset);
+
+    assertThat(paths.size(), is(3));
+
+    assertThat(paths, containsInAnyOrder(
+        new BrowsePath("packages", "/packages/"),
+        new BrowsePath("path", "/packages/path/"),
+        new BrowsePath("assetName.deb", "/packages/path/assetName.deb/")));
+  }
+
+  @Test
+  public void computeAssetPathSnapshots() {
+    AssetData asset = new AssetData();
+    asset.setPath("/snapshots/path/assetName");
+
+    List<BrowsePath> paths = generator.computeAssetPaths(asset);
+
+    assertThat(paths.size(), is(3));
+
+    assertThat(paths, containsInAnyOrder(
+        new BrowsePath("snapshots", "/snapshots/"),
+        new BrowsePath("path", "/snapshots/path/"),
+        new BrowsePath("assetName", "/snapshots/path/assetName/")));
+  }
+}
