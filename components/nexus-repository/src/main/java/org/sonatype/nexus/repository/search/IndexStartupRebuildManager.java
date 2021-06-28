4dd8a4e2a2db5cbd002b913fdf009916e4cd6ba3
sonatype-zion <zion@sonatype.com>
2021-06-28 10:26:05 -0400

    upstream: b=master,r=0195d2b222ab86bb313cc1768e9d85f5b45ca2bf,t=2021-06-28-0846-26703

---
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/search/IndexStartupRebuildManager.java
@@ -0,0 +1,77 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.search;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.goodies.lifecycle.LifecycleSupport;
+import org.sonatype.nexus.common.app.ManagedLifecycle;
+import org.sonatype.nexus.repository.search.index.RebuildIndexTaskDescriptor;
+import org.sonatype.nexus.scheduling.TaskConfiguration;
+import org.sonatype.nexus.scheduling.TaskScheduler;
+
+import static org.sonatype.nexus.common.app.ManagedLifecycle.Phase.TASKS;
+import static org.sonatype.nexus.repository.RepositoryTaskSupport.ALL_REPOSITORIES;
+import static org.sonatype.nexus.repository.RepositoryTaskSupport.REPOSITORY_NAME_FIELD_ID;
+
+/**
+ * This class handles the rebuilding of all Elasticsearch indexes on startup given a specified environment variable is
+ * set to true.
+ */
+@Named
+@Singleton
+@ManagedLifecycle(phase = TASKS)
+public class IndexStartupRebuildManager
+    extends LifecycleSupport
+{
+  public static final String REBUILD_ON_STARTUP_VARIABLE_NAME = "NEXUS_SEARCH_INDEX_REBUILD_ON_STARTUP";
+
+  private final TaskScheduler taskScheduler;
+
+  private final boolean rebuildOnStart;
+
+  @Inject
+  public IndexStartupRebuildManager(final TaskScheduler taskScheduler) {
+    this(taskScheduler, System.getenv(REBUILD_ON_STARTUP_VARIABLE_NAME));
+  }
+
+  public IndexStartupRebuildManager(final TaskScheduler taskScheduler, @Nullable final String rebuildOnStartEnvVar) {
+    this.taskScheduler = taskScheduler;
+    this.rebuildOnStart = Boolean.parseBoolean(rebuildOnStartEnvVar);
+  }
+
+  @Override
+  protected void doStart() throws Exception {
+    if (rebuildOnStart) {
+      log.info("Scheduling rebuild of repository indexes");
+      doRebuildAllIndexes();
+    }
+  }
+
+  /**
+   * Schedule one-off background task to rebuild the indexes of all repositories.
+   */
+  private void doRebuildAllIndexes() {
+    try {
+      TaskConfiguration taskConfig = taskScheduler.createTaskConfigurationInstance(RebuildIndexTaskDescriptor.TYPE_ID);
+      taskConfig.setString(REPOSITORY_NAME_FIELD_ID, ALL_REPOSITORIES);
+      taskScheduler.submit(taskConfig);
+    }
+    catch (RuntimeException e) {
+      log.warn("Problem scheduling rebuild of repository indexes", e);
+    }
+  }
+}
---
+++ components/nexus-repository/src/test/java/org/sonatype/nexus/repository/search/IndexStartupRebuildManagerTest.java
@@ -0,0 +1,73 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.search;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.repository.search.index.RebuildIndexTaskDescriptor;
+import org.sonatype.nexus.scheduling.TaskConfiguration;
+import org.sonatype.nexus.scheduling.TaskScheduler;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+public class IndexStartupRebuildManagerTest
+    extends TestSupport
+{
+  private IndexStartupRebuildManager underTest;
+
+  @Mock
+  TaskScheduler taskScheduler;
+
+  @Test
+  public void rebuildDoesntRunIfNoVariableIsSet() throws Exception {
+    underTest = new IndexStartupRebuildManager(taskScheduler, null);
+    underTest.doStart();
+    verify(taskScheduler, never()).submit(any());
+  }
+
+  @Test
+  public void rebuildDoesntRunIfVariableIsFalse() throws Exception {
+    underTest = new IndexStartupRebuildManager(taskScheduler, "false");
+    underTest.doStart();
+    verify(taskScheduler, never()).submit(any());
+  }
+
+  @Test
+  public void rebuildDoesntRunIfVariableIsInvalid() throws Exception {
+    underTest = new IndexStartupRebuildManager(taskScheduler, "this is invalid");
+    underTest.doStart();
+    verify(taskScheduler, never()).submit(any());
+  }
+
+  @Test
+  public void rebuildDoesRunIfVariableIsTrue() throws Exception {
+    underTest = new IndexStartupRebuildManager(taskScheduler, "true");
+    when(taskScheduler.createTaskConfigurationInstance(RebuildIndexTaskDescriptor.TYPE_ID))
+        .thenReturn(new TaskConfiguration());
+    underTest.doStart();
+
+    ArgumentCaptor<TaskConfiguration> taskConfigurationArgumentCaptor =
+        ArgumentCaptor.forClass(TaskConfiguration.class);
+
+    verify(taskScheduler).submit(taskConfigurationArgumentCaptor.capture());
+
+    TaskConfiguration taskConfiguration = taskConfigurationArgumentCaptor.getValue();
+
+    assertThat(taskConfiguration.getString("repositoryName"), is( "*"));
+  }
+}
