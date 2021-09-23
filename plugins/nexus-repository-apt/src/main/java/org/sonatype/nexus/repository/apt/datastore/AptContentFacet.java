ad780c00ca7e41ebd5a6be44540836051076738a
Matthew Piggott <mpiggott@sonatype.com>
2021-09-23 14:12:56 -0400

    Public export of Nexus code:   - d96e82853a53c53cfa859a82d5220c2fa60c1e34 by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: d96e82853a53c53cfa859a82d5220c2fa60c1e34

--- plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/AptContentFacet.java
+++ plugins/nexus-repository-apt/src/main/java/org/sonatype/nexus/repository/apt/datastore/AptContentFacet.java
@@ -14,55 +14,17 @@ package org.sonatype.nexus.repository.apt.datastore;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 import javax.annotation.Nullable;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.validation.constraints.NotNull;
 
 import org.sonatype.nexus.repository.Facet;
-import org.sonatype.nexus.repository.apt.internal.AptFacetHelper;
-import org.sonatype.nexus.repository.apt.AptFormat;
-import org.sonatype.nexus.repository.apt.internal.AptPackageParser;
-import org.sonatype.nexus.repository.apt.internal.debian.ControlFile;
 import org.sonatype.nexus.repository.apt.internal.debian.PackageInfo;
-import org.sonatype.nexus.repository.config.Configuration;
-import org.sonatype.nexus.repository.config.ConfigurationFacet;
-import org.sonatype.nexus.repository.config.WritePolicy;
-import org.sonatype.nexus.repository.content.Asset;
-import org.sonatype.nexus.repository.content.AssetBlob;
-import org.sonatype.nexus.repository.content.facet.ContentFacetSupport;
+import org.sonatype.nexus.repository.content.facet.ContentFacet;
 import org.sonatype.nexus.repository.content.fluent.FluentAsset;
-import org.sonatype.nexus.repository.content.fluent.FluentComponent;
-import org.sonatype.nexus.repository.content.store.AssetDAO;
-import org.sonatype.nexus.repository.content.store.FormatStoreManager;
-import org.sonatype.nexus.repository.content.utils.FormatAttributesUtils;
-import org.sonatype.nexus.repository.types.HostedType;
-import org.sonatype.nexus.repository.types.ProxyType;
 import org.sonatype.nexus.repository.view.Content;
 import org.sonatype.nexus.repository.view.Payload;
 import org.sonatype.nexus.repository.view.payloads.TempBlob;
-
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableMap;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static org.sonatype.nexus.common.entity.Continuations.iterableOf;
-import static org.sonatype.nexus.common.hash.HashAlgorithm.MD5;
-import static org.sonatype.nexus.common.hash.HashAlgorithm.SHA1;
-import static org.sonatype.nexus.common.hash.HashAlgorithm.SHA256;
-import static org.sonatype.nexus.repository.apt.internal.AptFacetHelper.normalizeAssetPath;
-import static org.sonatype.nexus.repository.apt.internal.AptProperties.DEB;
-import static org.sonatype.nexus.repository.apt.internal.AptProperties.P_ARCHITECTURE;
-import static org.sonatype.nexus.repository.apt.internal.AptProperties.P_INDEX_SECTION;
-import static org.sonatype.nexus.repository.apt.internal.AptProperties.P_PACKAGE_NAME;
-import static org.sonatype.nexus.repository.apt.internal.AptProperties.P_PACKAGE_VERSION;
-import static org.sonatype.nexus.repository.apt.debian.Utils.isDebPackageContentType;
-import static org.sonatype.nexus.repository.storage.AssetEntityAdapter.P_ASSET_KIND;
 
 /**
  * Apt content facet
@@ -70,171 +32,26 @@ import static org.sonatype.nexus.repository.storage.AssetEntityAdapter.P_ASSET_K
  * @since 3.31
  */
 @Facet.Exposed
-@Named(AptFormat.NAME)
-public class AptContentFacet
-    extends ContentFacetSupport
+public interface AptContentFacet
+    extends ContentFacet
 {
-  @VisibleForTesting
-  static final String CONFIG_KEY = "apt";
+  String getDistribution();
 
-  @Inject
-  public AptContentFacet(
-      @Named(AptFormat.NAME) final FormatStoreManager formatStoreManager)
-  {
-    super(formatStoreManager);
-  }
+  boolean isFlat();
 
-  static class Config
-  {
-    @NotNull(groups = {HostedType.ValidationGroup.class, ProxyType.ValidationGroup.class})
-    public String distribution;
+  Optional<FluentAsset> getAsset(String path);
 
-    @NotNull(groups = {ProxyType.ValidationGroup.class})
-    public boolean flat;
-  }
+  Optional<Content> get(String assetPath);
 
-  private Config config;
+  FluentAsset put(String path, Payload content) throws IOException;
 
-  @Override
-  protected WritePolicy writePolicy(final Asset asset) {
-    WritePolicy writePolicy = super.writePolicy(asset);
-    if (WritePolicy.ALLOW_ONCE == writePolicy) {
-      String name = asset.path();
-      if (name.endsWith(".deb")) {
-        return WritePolicy.ALLOW_ONCE;
-      }
-      else {
-        return WritePolicy.ALLOW;
-      }
-    }
-    return writePolicy;
-  }
+  FluentAsset put(String path, Payload payload, @Nullable PackageInfo packageInfo) throws IOException;
 
-  @Override
-  protected void doConfigure(final Configuration configuration) throws Exception {
-    super.doConfigure(configuration);
-    config = facet(ConfigurationFacet.class)
-        .readSection(configuration, CONFIG_KEY, Config.class);
-    log.debug("APT config: {}", config);
-  }
+  FluentAsset findOrCreateMetadataAsset(TempBlob tempBlob, String path);
 
-  public String getDistribution() {
-    return config.distribution;
-  }
+  TempBlob getTempBlob(Payload payload);
 
-  public boolean isFlat() {
-    return config.flat;
-  }
+  TempBlob getTempBlob(InputStream in, @Nullable String contentType);
 
-  public Optional<FluentAsset> getAsset(final String path) {
-    return assets().path(normalizeAssetPath(path)).find();
-  }
-
-  public Optional<Content> get(final String assetPath) {
-    return assets().path(normalizeAssetPath(assetPath)).find().map(FluentAsset::download);
-  }
-
-  public FluentAsset put(final String path, final Payload content) throws IOException {
-    return put(path, content, null);
-  }
-
-  public FluentAsset put(final String path,
-                         final Payload payload,
-                         @Nullable final PackageInfo packageInfo) throws IOException
-  {
-    String normalizedPath = normalizeAssetPath(path);
-
-    try (TempBlob tempBlob = blobs().ingest(payload, AptFacetHelper.hashAlgorithms)) {
-      return isDebPackageContentType(normalizedPath)
-          ? findOrCreateDebAsset(normalizedPath, tempBlob, packageInfo)
-          : findOrCreateMetadataAsset(tempBlob, normalizedPath);
-    }
-  }
-
-  private FluentAsset findOrCreateDebAsset(final String path, final TempBlob tempBlob, @Nullable PackageInfo packageInfo)
-      throws IOException
-  {
-    if (packageInfo == null) {
-      packageInfo = AptPackageParser.parsePackageInfo(tempBlob);
-    }
-
-    FluentAsset asset = assets()
-        .path(normalizeAssetPath(path))
-        .kind(DEB)
-        .component(findOrCreateComponent(packageInfo))
-        .blob(tempBlob)
-        .save();
-
-    ControlFile controlFile = packageInfo.getControlFile();
-    populateAttributes(packageInfo, asset, controlFile);
-
-    return asset;
-  }
-
-  private void populateAttributes(final PackageInfo info, final FluentAsset asset, final ControlFile controlFile) {
-    final Map<String, Object> formatAttributes = new HashMap<>();
-    formatAttributes.put(P_ARCHITECTURE, info.getArchitecture());
-    formatAttributes.put(P_PACKAGE_NAME, info.getPackageName());
-    formatAttributes.put(P_PACKAGE_VERSION, info.getVersion());
-    formatAttributes.put(P_INDEX_SECTION, buildIndexSection(controlFile, asset));
-    formatAttributes.put(P_ASSET_KIND, DEB);
-
-    FormatAttributesUtils.setFormatAttributes(asset, formatAttributes);
-  }
-
-  private String buildIndexSection(final ControlFile controlFile, final FluentAsset asset) {
-    AssetBlob assetBlob = asset.blob()
-        .orElseThrow(() -> new IllegalStateException(
-            "Impossible build " + P_INDEX_SECTION + ". Asset blob couldn't be found for asset: " + asset.path()));
-    final Map<String, String> checksums = assetBlob.checksums();
-
-    return controlFile.getParagraphs().get(0)
-        .withFields(Arrays.asList(
-            new ControlFile.ControlField("Filename", asset.path()),
-            new ControlFile.ControlField("Size", Long.toString(assetBlob.blobSize())),
-            new ControlFile.ControlField("MD5Sum", checksums.get(MD5.name())),
-            new ControlFile.ControlField("SHA1", checksums.get(SHA1.name())),
-            new ControlFile.ControlField("SHA256", checksums.get(SHA256.name()))))
-        .toString();
-  }
-
-  public FluentAsset findOrCreateMetadataAsset(final TempBlob tempBlob, final String path) {
-    return assets()
-        .path(normalizeAssetPath(path))
-        .blob(tempBlob)
-        .save();
-  }
-
-  private FluentComponent findOrCreateComponent(final PackageInfo info) {
-    String name = info.getPackageName();
-    String version = info.getVersion();
-    String architecture = info.getArchitecture();
-
-    return components()
-        .name(name)
-        .version(version)
-        .namespace(architecture)
-        .getOrCreate();
-  }
-
-  public TempBlob getTempBlob(final Payload payload) {
-    checkNotNull(payload);
-    return blobs().ingest(payload, AptFacetHelper.hashAlgorithms);
-  }
-
-  public TempBlob getTempBlob(final InputStream in, @Nullable final String contentType) {
-    checkNotNull(in);
-    return blobs().ingest(in, contentType, AptFacetHelper.hashAlgorithms);
-  }
-
-  public void deleteAssetsByPrefix(final String pathPrefix) {
-    String filter = "repository_id = #{" + AssetDAO.FILTER_PARAMS + ".repositoryParam}" +
-        " AND path LIKE #{" + AssetDAO.FILTER_PARAMS + ".pathParam}" +
-        " AND component_id IS NULL";
-
-    Map<String, Object> params = ImmutableMap.of("repositoryParam", contentRepositoryId(),
-        "pathParam", pathPrefix + "%");
-
-    iterableOf(assets().byFilter(filter, params)::browse).forEach(FluentAsset::delete);
-  }
+  void deleteAssetsByPrefix(String pathPrefix);
 }
