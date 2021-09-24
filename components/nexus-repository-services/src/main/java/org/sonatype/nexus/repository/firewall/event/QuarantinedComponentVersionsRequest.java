2ca24857f6e297faf1d4361df1a5d075fbb172c0
Dennis Warren <dwarren@sonatype.com>
2021-09-24 16:10:50 -0600

    Public export of Nexus code:   - 719489767fff3ef5742ad8ae2343c9fb56d046e9 by Dennis Warren <dwarren@sonatype.com>
    GitOrigin-RevId: 719489767fff3ef5742ad8ae2343c9fb56d046e9

---
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/firewall/event/QuarantinedComponentVersionsRequest.java
@@ -0,0 +1,50 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.firewall.event;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+import org.sonatype.nexus.repository.vulnerability.AuditRepositoryComponent;
+
+/**
+ * @since 3.next
+ */
+public class QuarantinedComponentVersionsRequest
+{
+  private final String repositoryName;
+
+  private final Set<AuditRepositoryComponent> repositoryComponents;
+
+  private final CompletableFuture<QuarantinedComponentVersions> quarantinedResult = new CompletableFuture<>();
+
+  public QuarantinedComponentVersionsRequest(
+      final String repositoryName,
+      final Set<AuditRepositoryComponent> repositoryComponents)
+  {
+    this.repositoryName = repositoryName;
+    this.repositoryComponents = repositoryComponents;
+  }
+
+  public String getRepositoryName() {
+    return repositoryName;
+  }
+
+  public Set<AuditRepositoryComponent> getRepositoryComponents() {
+    return repositoryComponents;
+  }
+
+  public CompletableFuture<QuarantinedComponentVersions> getQuarantinedResult() {
+    return quarantinedResult;
+  }
+}
