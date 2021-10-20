16e712e4fafb91e35e5ac4a590f93e4af3e09f63
Matthew Piggott <mpiggott@sonatype.com>
2021-10-20 09:48:02 -0400

    Public export of Nexus code:   - 3cddeb669e03cba4aeb65494f3c2c61b0a22bc5c by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: 3cddeb669e03cba4aeb65494f3c2c61b0a22bc5c

--- components/nexus-blobstore/src/main/java/org/sonatype/nexus/blobstore/group/BlobStoreGroup.java
+++ components/nexus-blobstore/src/main/java/org/sonatype/nexus/blobstore/group/BlobStoreGroup.java
@@ -133,7 +133,6 @@ public class BlobStoreGroup
           fillPolicyName, configuration.getName(), FALLBACK_FILL_POLICY_TYPE);
       this.fillPolicy = fillPolicyProviders.get(FALLBACK_FILL_POLICY_TYPE).get();
     }
-    fillPolicy.validateBlobStoreGroup(this);
   }
 
   @Override
@@ -186,7 +185,7 @@ public class BlobStoreGroup
   }
 
   private Blob create(final Map<String, String> headers, final CreateBlobFunction createBlobFunction) {
-    BlobStore result = fillPolicy.chooseBlobStoreForCreate(this, headers);
+    BlobStore result = fillPolicy.chooseBlobStore(this, headers);
     if (result == null) {
       throw new BlobStoreException("Unable to find a member Blob Store of '" + this + "' for create", null);
     }
@@ -198,32 +197,10 @@ public class BlobStoreGroup
   @Override
   @Guarded(by = STARTED)
   public Blob copy(final BlobId blobId, final Map<String, String> headers) {
-    BlobStore sourceBlobStore = locate(blobId)
+    BlobStore target = locate(blobId)
         .orElseThrow(() -> new BlobStoreException("Unable to find blob", blobId));
-
-    BlobStore destinationBlobStore = fillPolicy.chooseBlobStoreForCopy(this, sourceBlobStore, headers);
-
-    if (destinationBlobStore == null) {
-      throw new BlobStoreException("Unable to find a member Blob Store of '" + this + "' for copy", blobId);
-    }
-
-    Blob blob;
-    //if the source and destination blob stores are the same, perform the copy
-    if (sourceBlobStore.getBlobStoreConfiguration().getName()
-        .equals(destinationBlobStore.getBlobStoreConfiguration().getName())) {
-      blob = sourceBlobStore.copy(blobId, headers);
-    }
-    //if the source and destination blob stores are different, create it the blob in the destination store
-    else {
-      Blob blobToCopy = sourceBlobStore.get(blobId);
-      if (blobToCopy == null) {
-        log.error("Unable to copy blob because it doesn't exist in blob store: {}, blobId: {}", sourceBlobStore.getBlobStoreConfiguration().getName(), blobId);
-        throw new BlobStoreException("Unable to blob to copy", blobId);
-      }
-      blob = destinationBlobStore.create(blobToCopy.getInputStream(), headers, null);
-    }
-
-    locatedBlobs.put(blob.getId(), destinationBlobStore.getBlobStoreConfiguration().getName());
+    Blob blob = target.copy(blobId, headers);
+    locatedBlobs.put(blob.getId(), target.getBlobStoreConfiguration().getName());
     return blob;
   }
 
