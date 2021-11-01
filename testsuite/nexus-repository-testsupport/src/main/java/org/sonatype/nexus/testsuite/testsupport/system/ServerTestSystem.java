a766c8b985e853307f8fdd5a12870373442df401
Damian Bradicich <dbradicich@sonatype.com>
2021-11-01 07:38:56 -0400

    Public export of Nexus code:   - 8becc82cb8e55d6ed60152d33a54faf0b2b19314 by Damian Bradicich <dbradicich@sonatype.com>
    GitOrigin-RevId: 8becc82cb8e55d6ed60152d33a54faf0b2b19314

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/ServerTestSystem.java
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/ServerTestSystem.java
@@ -10,29 +10,43 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.testsuite.testsupport.fixtures;
+package org.sonatype.nexus.testsuite.testsupport.system;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
 import org.sonatype.goodies.httpfixture.server.api.Behaviour;
 import org.sonatype.goodies.httpfixture.server.fluent.Server;
+import org.sonatype.nexus.common.event.EventManager;
 import org.sonatype.nexus.common.net.PortAllocator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.apache.commons.lang3.StringUtils.prependIfMissing;
 
-/**
- * @deprecated in favor of ServerTestSystem in the new IT Framework
- */
-@Deprecated()
-public class ServerRule
-    extends ExternalResourceSupport
+@Named
+@Singleton
+public class ServerTestSystem
+    extends TestSystemSupport
 {
-  List<Server> servers = new ArrayList<>();
+  private static final Logger log = LoggerFactory.getLogger(ServerTestSystem.class);
+
+  List<Server> servers;
+
+  @Inject
+  public ServerTestSystem(final EventManager eventManager) {
+    super(eventManager);
+    servers = new ArrayList<>();
+  }
 
   @Override
-  protected void after() {
+  protected void doAfter() {
     for (Server server : servers) {
       try {
         server.stop();
@@ -44,12 +58,7 @@ public class ServerRule
   }
 
   public Server createServer(final Map<String, Behaviour> behaviors) throws Exception {
-    int port = PortAllocator.nextFreePort();
-    return createServer(port, behaviors);
-  }
-
-  public Server createServer(final int port, final Map<String, Behaviour> behaviors) throws Exception {
-    Server server = Server.withPort(port);
+    Server server = Server.withPort(PortAllocator.nextFreePort());
     behaviors.forEach((key, value) -> server.serve(prependIfMissing(key, "/")).withBehaviours(value));
     servers.add(server);
     server.start();
