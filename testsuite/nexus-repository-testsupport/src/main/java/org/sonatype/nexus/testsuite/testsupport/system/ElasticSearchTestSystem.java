d816217668b0815fde49dd8fc9344a1e2219e9f3
Sergii Pryhoda <userseprid@gmail.com>
2022-02-28 10:32:52 +0200

    Public export of Nexus code:   - 9aee721dd74671c2324ef6798915774e0871e76d by Sergii Pryhoda <userseprid@gmail.com>
    GitOrigin-RevId: 9aee721dd74671c2324ef6798915774e0871e76d

---
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/ElasticSearchTestSystem.java
@@ -0,0 +1,56 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.testsuite.testsupport.system;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.common.event.EventManager;
+import org.sonatype.nexus.repository.search.index.ElasticSearchIndexService;
+import org.sonatype.nexus.repository.search.query.ElasticSearchQueryService;
+
+import static org.awaitility.Awaitility.await;
+
+/**
+ * @since 3.next
+ */
+@Named
+@Singleton
+public class ElasticSearchTestSystem
+    implements SearchTestSystem
+{
+
+  @Inject
+  public EventManager eventManager;
+
+  @Inject
+  public ElasticSearchIndexService indexService;
+
+  @Inject
+  public ElasticSearchQueryService elasticSearchQueryService;
+
+  @Override
+  public void waitForSearch() {
+    await().atMost(30, TimeUnit.SECONDS).until(eventManager::isCalmPeriod);
+    indexService.flush(false); // no need for full fsync here
+    await().atMost(30, TimeUnit.SECONDS).until(indexService::isCalmPeriod);
+  }
+
+  public ElasticSearchQueryService queryService() {
+    return elasticSearchQueryService;
+  }
+
+}
