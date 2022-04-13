08796bf007360387d9bcc0f66332b8194ce8fc6b
Vitaly Gashock <gshockv@gmail.com>
2022-04-13 16:47:19 +0300

    Public export of Nexus code:   - 140a1b0d001d8adf2b9a99b5bc743be4493f540c by Vitaly Gashock <gshockv@gmail.com>
    GitOrigin-RevId: 140a1b0d001d8adf2b9a99b5bc743be4493f540c

--- components/nexus-repository-view/src/main/java/org/sonatype/nexus/repository/http/HttpConditions.java
+++ components/nexus-repository-view/src/main/java/org/sonatype/nexus/repository/http/HttpConditions.java
@@ -12,27 +12,33 @@
  */
 package org.sonatype.nexus.repository.http;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import org.sonatype.nexus.repository.view.Context;
 import org.sonatype.nexus.repository.view.Headers;
 import org.sonatype.nexus.repository.view.Request;
 import org.sonatype.nexus.repository.view.Response;
+import org.sonatype.nexus.repository.view.Status;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import com.google.common.base.Strings;
 import com.google.common.net.HttpHeaders;
 import org.apache.http.client.utils.DateUtils;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.repository.http.HttpMethods.GET;
+import static org.sonatype.nexus.repository.http.HttpMethods.HEAD;
+import static org.sonatype.nexus.repository.http.HttpStatus.NOT_MODIFIED;
+import static org.sonatype.nexus.repository.http.HttpStatus.PRECONDITION_FAILED;
 
 /**
  * Helper defining common HTTP conditions.
@@ -41,6 +47,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class HttpConditions
 {
+  private static final Logger log = LoggerFactory.getLogger(HttpConditions.class);
+
   /**
    * Request attribute containing the stashed conditions.
    */
@@ -58,6 +66,17 @@ public class HttpConditions
 
   private HttpConditions() {
     // empty
+  }
+
+  /**
+   * Verifiers whether a {@link Request} contains conditional headers.
+   *
+   * @return {@code true} if the request contains conditional headers, {@code false} otherwise.
+   */
+  public static boolean isConditional(final Request request) {
+    Set<String> headerNames = request.getHeaders().names();
+    return headerNames.contains(HttpHeaders.IF_MODIFIED_SINCE) || headerNames.contains(HttpHeaders.IF_UNMODIFIED_SINCE)
+        || headerNames.contains(HttpHeaders.IF_MATCH) || headerNames.contains(HttpHeaders.IF_NONE_MATCH);
   }
 
   /**
@@ -87,149 +106,129 @@ public class HttpConditions
   @Nonnull
   public static Request makeConditional(@Nonnull final Request request) {
     checkNotNull(request);
-    final Headers stashedHeaders = request.getAttributes().require(HTTP_CONDITIONS, Headers.class);
+    final Headers stashedHeaders = getConditionalHeaders(request);
     for (Entry<String, String> entry : stashedHeaders.entries()) {
       request.getHeaders().set(entry.getKey(), stashedHeaders.getAll(entry.getKey()));
     }
     return request;
   }
 
-  /**
-   * Builds a {@link Predicate} that contains conditions in passed in {@link Request} or {@code null} if
-   * request does not contains any condition. The predicate applies to {@link Response} if it meets all the conditions
-   * found (they all are logically AND bound).
+  /*
+   * Retrieves cached conditional headers moved by {@link #makeUnconditional}
    */
-  @Nullable
-  public static Predicate<Response> requestPredicate(@Nonnull final Request request) {
-    checkNotNull(request);
-    final List<Predicate<Response>> predicates = new ArrayList<>();
-    final Predicate<Response> ifModifiedSince = ifModifiedSince(request);
-    if (ifModifiedSince != null) {
-      predicates.add(ifModifiedSince);
-    }
-    final Predicate<Response> ifUnmodifiedSince = ifUnmodifiedSince(request);
-    if (ifUnmodifiedSince != null) {
-      predicates.add(ifUnmodifiedSince);
-    }
-    final Predicate<Response> ifMatch = ifMatch(request);
-    if (ifMatch != null) {
-      predicates.add(ifMatch);
-    }
-    final Predicate<Response> ifNoneMatch = ifNoneMatch(request);
-    if (ifNoneMatch != null) {
-      predicates.add(ifNoneMatch);
-    }
-
-    if (!predicates.isEmpty()) {
-      return Predicates.and(predicates);
-    }
-    return null;
+  private static Headers getConditionalHeaders(final Request request) {
+    return request.getAttributes().require(HTTP_CONDITIONS, Headers.class);
   }
 
-  @Nullable
-  private static Predicate<Response> ifModifiedSince(final Request request) {
-    final DateTime date = parseDateHeader(request.getHeaders().get(HttpHeaders.IF_MODIFIED_SINCE));
-    if (date != null) {
-      return new Predicate<Response>()
-      {
-        @Override
-        public boolean apply(final Response response) {
-          final DateTime lastModified = parseDateHeader(response.getHeaders().get(HttpHeaders.LAST_MODIFIED));
-          if (lastModified != null) {
-            return lastModified.isAfter(date);
-          }
-          return true;
-        }
+  /**
+   * Creates a conditional {@link Response} if the provided {@code context} and provided {@code response} from a GET
+   * request.
+   *
+   * <a href="See https://httpwg.org/specs/rfc7232.html#precedence">RFC 7232</a>
+   *
+   * @param context the context for the request
+   * @param getResponse a response from a GET request
+   * @return an Optional containing the conditional response, or an empty response
+   */
+  public static Optional<Response> maybeCreateConditionalResponse(
+      final Context context,
+      final Response getResponse)
+  {
+    // Request headers
+    Headers headers = getConditionalHeaders(context.getRequest());
+    Optional<String> ifMatchHeader = Optional.ofNullable(headers.get(HttpHeaders.IF_MATCH));
+    Optional<String> ifUnmodifiedHeader = Optional.ofNullable(headers.get(HttpHeaders.IF_UNMODIFIED_SINCE));
+    Optional<String> ifNoneMatchHeader = Optional.ofNullable(headers.get(HttpHeaders.IF_NONE_MATCH));
+    Optional<String> ifModifiedHeader = Optional.ofNullable(headers.get(HttpHeaders.IF_MODIFIED_SINCE));
 
-        @Override
-        public String toString() {
-          return HttpConditions.class.getSimpleName() + ".ifModifiedSince(" + date + ")";
-        }
-      };
-    }
-    return null;
-  }
+    Optional<DateTime> lastModified = parseDateHeader(getResponse.getHeaders().get(HttpHeaders.LAST_MODIFIED));
 
-  @Nullable
-  private static Predicate<Response> ifUnmodifiedSince(final Request request) {
-    final DateTime date = parseDateHeader(request.getHeaders().get(HttpHeaders.IF_UNMODIFIED_SINCE));
-    if (date != null) {
-      return new Predicate<Response>()
-      {
-        @Override
-        public boolean apply(final Response response) {
-          final DateTime lastModified = parseDateHeader(response.getHeaders().get(HttpHeaders.LAST_MODIFIED));
-          if (lastModified != null) {
-            return !lastModified.isAfter(date);
-          }
-          return true;
-        }
-
-        @Override
-        public String toString() {
-          return HttpConditions.class.getSimpleName() + ".ifUnmodifiedSince(" + date + ")";
-        }
-      };
-    }
-    return null;
-  }
-
-  @Nullable
-  private static Predicate<Response> ifMatch(final Request request) {
-    final String match = request.getHeaders().get(HttpHeaders.IF_MATCH);
-    if (match != null && !"*".equals(match)) {
-      return new Predicate<Response>()
-      {
-        @Override
-        public boolean apply(final Response response) {
-          final String etag = response.getHeaders().get(HttpHeaders.ETAG);
-          if (etag != null) {
-            return match.contains(etag);
-          }
-          return true;
-        }
-
-        @Override
-        public String toString() {
-          return HttpConditions.class.getSimpleName() + ".ifMatch(" + match + ")";
-        }
-      };
-    }
-    return null;
-  }
-
-  @Nullable
-  private static Predicate<Response> ifNoneMatch(final Request request) {
-    final String match = request.getHeaders().get(HttpHeaders.IF_NONE_MATCH);
-    if (match != null && !"*".equals(match)) {
-      return new Predicate<Response>()
-      {
-        @Override
-        public boolean apply(final Response response) {
-          final String etag = response.getHeaders().get(HttpHeaders.ETAG);
-          if (etag != null) {
-            return !match.contains(etag);
-          }
-          return true;
-        }
-
-        @Override
-        public String toString() {
-          return HttpConditions.class.getSimpleName() + ".ifNoneMatch(" + match + ")";
-        }
-      };
-    }
-    return null;
-  }
-
-  @Nullable
-  private static DateTime parseDateHeader(@Nullable final String httpDate) {
-    if (!Strings.isNullOrEmpty(httpDate)) {
-      final Date date = DateUtils.parseDate(httpDate);
-      if (date != null) {
-        return new DateTime(date.getTime());
+    if (ifMatchHeader.isPresent()) {
+      // Step 1
+      if (!HttpConditions.doesEtagMatch(ifMatchHeader.get(), getResponse)) {
+        log.debug("ETag: {} does not If-Unmodified: {}", getResponse.getHeaders().get(HttpHeaders.ETAG),
+            ifMatchHeader.get());
+        return Optional.of(buildPreconditionFailed(getResponse));
       }
     }
-    return null;
+    else if (ifUnmodifiedHeader.isPresent() && lastModified.isPresent()) {
+      // While the header is present, the date may be invalid so we keep it as Optional
+      boolean modifiedAfterSpecifiedDate = ifUnmodifiedHeader.flatMap(HttpConditions::parseDateHeader)
+          .map(lastModified.get()::isAfter)
+          .orElse(false);
+      if (modifiedAfterSpecifiedDate) {
+        log.debug("Precondition Failed - LastModified {} is after If-Unmodified-Since {}", lastModified.get(),
+            ifUnmodifiedHeader.get());
+        return Optional.of(buildPreconditionFailed(getResponse));
+      }
+    }
+
+    if (ifNoneMatchHeader.isPresent()) {
+      // Step 3
+      if (HttpConditions.doesEtagMatch(ifNoneMatchHeader.get(), getResponse)) {
+        log.debug("ETag: {} does not If-None-Match: {}", getResponse.getHeaders().get(HttpHeaders.ETAG),
+            ifNoneMatchHeader.get());
+        // 304 for get/head, 412 for post/put (not really applicable)
+        return Optional.of(getOrHead(context.getRequest()) ? buildNotModified(getResponse)
+            : buildPreconditionFailed(getResponse));
+      }
+    }
+    else if (ifModifiedHeader.isPresent() && lastModified.isPresent() && getOrHead(context.getRequest())) {
+      // Step 4
+      // While the header is present, the date may be invalid so we keep it as Optional
+      boolean modifiedAfterSpecifiedDate = ifModifiedHeader.flatMap(HttpConditions::parseDateHeader)
+          .map(lastModified.get()::isAfter)
+          .orElse(true);
+      if (!modifiedAfterSpecifiedDate) {
+        log.debug("Not Modified - LastModified {} is after If-Modified-Since {}", lastModified.get(),
+            ifModifiedHeader.get());
+        return Optional.of(buildNotModified(getResponse));
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  private static boolean getOrHead(final Request request) {
+    String action = request.getAction();
+    return GET.equals(action) || HEAD.equals(action);
+  }
+
+  private static Response buildPreconditionFailed(final Response response) {
+    return new Response.Builder()
+        .copy(response)
+        .status(Status.failure(PRECONDITION_FAILED))
+        .payload(null)
+        .build();
+  }
+
+  private static Response buildNotModified(final Response response) {
+    // copy only ETag header, leave out all other entity headers
+    final Response.Builder responseBuilder = new Response.Builder().status(Status.success(NOT_MODIFIED));
+    Optional.ofNullable(response.getHeaders().get(HttpHeaders.ETAG))
+        .ifPresent(eTag -> responseBuilder.header(HttpHeaders.ETAG, eTag));
+    return responseBuilder.build();
+  }
+
+  /**
+   * Returns true if a match header matches the eTag of the response
+   *
+   * @param matchHeader a match header
+   * @param response the response
+   */
+  private static boolean doesEtagMatch(final String matchHeader, final Response response) {
+    if ("*".equals(matchHeader)) {
+      return true;
+    }
+    return Optional.ofNullable(response.getHeaders().get(HttpHeaders.ETAG))
+        .map(matchHeader::contains)
+        .orElse(false);
+  }
+
+  private static Optional<DateTime> parseDateHeader(@Nullable final String httpDate) {
+    return Optional.ofNullable(httpDate)
+        .map(DateUtils::parseDate)
+        .map(Date::getTime)
+        .map(DateTime::new);
   }
 }
