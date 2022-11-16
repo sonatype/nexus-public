2aa624791764ba0cbfb15dd11de74b507630cece
Michael Martz <mmartz@sonatype.com>
2022-11-16 16:22:11 -0600

    Public export of Nexus code:   - 07cb093f8f40d60a518e48d4eab63438dac323a2 by Michael Martz <mmartz@sonatype.com>
    GitOrigin-RevId: 07cb093f8f40d60a518e48d4eab63438dac323a2

--- plugins/nexus-coreui-plugin/src/frontend/src/components/pages/admin/SupportZip/SupportZipForm.jsx
+++ plugins/nexus-coreui-plugin/src/frontend/src/components/pages/admin/SupportZip/SupportZipForm.jsx
@@ -12,110 +12,115 @@
  */
 import React from 'react';
 import UIStrings from '../../../../constants/UIStrings';
-import {NxButton, NxCheckbox, NxFieldset, NxFontAwesomeIcon} from '@sonatype/react-shared-components';
+import {NxButton, NxCheckbox, NxFieldset, NxFontAwesomeIcon, NxModal} from '@sonatype/react-shared-components';
 import {faFileArchive} from '@fortawesome/free-solid-svg-icons';
 
 export default function SupportZipForm({params, setParams, submit, clustered, hazips, cancel}) {
-  return <div className="nx-form">
-    {cancel
-        && <div className='nx-support-zip-modal-form-vertical-spacer' />
-    }
+  return <>
+    <NxModal.Content>
+      <div className="nx-form">
+        {cancel
+        && <div className='nx-support-zip-modal-form-vertical-spacer'/>
+        }
 
-    <div dangerouslySetInnerHTML={{__html: UIStrings.SUPPORT_ZIP.DESCRIPTION}}/>
-    <NxFieldset label={UIStrings.SUPPORT_ZIP.CONTENTS}>
-      <NxCheckbox
-          checkboxId='systemInformation'
-          isChecked={params.systemInformation}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.REPORT_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='threadDump'
-          isChecked={params.threadDump}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.DUMP_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='configuration'
-          isChecked={params.configuration}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.CONFIGURATION_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='security'
-          isChecked={params.security}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.SECURITY_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='log'
-          isChecked={params.log}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.LOGFILES_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='taskLog'
-          isChecked={params.taskLog}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.TASKLOGFILES_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='auditLog'
-          isChecked={params.auditLog}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.AUDITLOGFILES_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='metrics'
-          isChecked={params.metrics}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.METRICS_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='jmx'
-          isChecked={params.jmx}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.JMX_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='replication'
-          isChecked={params.replication}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.REPLICATION_LABEL}
-      </NxCheckbox>
-    </NxFieldset>
-    <NxFieldset label={UIStrings.SUPPORT_ZIP.OPTIONS}>
-      <NxCheckbox
-          checkboxId='limitFileSizes'
-          isChecked={params.limitFileSizes}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.LIMITFILESIZES_LABEL}
-      </NxCheckbox>
-      <NxCheckbox
-          checkboxId='limitZipSize'
-          isChecked={params.limitZipSize}
-          onChange={setParams}
-      >
-        {UIStrings.SUPPORT_ZIP.LIMITZIPSIZE_LABEL}
-      </NxCheckbox>
-    </NxFieldset>
+        <div dangerouslySetInnerHTML={{__html: UIStrings.SUPPORT_ZIP.DESCRIPTION}}/>
+        <NxFieldset label={UIStrings.SUPPORT_ZIP.CONTENTS}>
+          <NxCheckbox
+              checkboxId='systemInformation'
+              isChecked={params.systemInformation}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.REPORT_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='threadDump'
+              isChecked={params.threadDump}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.DUMP_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='configuration'
+              isChecked={params.configuration}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.CONFIGURATION_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='security'
+              isChecked={params.security}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.SECURITY_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='log'
+              isChecked={params.log}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.LOGFILES_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='taskLog'
+              isChecked={params.taskLog}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.TASKLOGFILES_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='auditLog'
+              isChecked={params.auditLog}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.AUDITLOGFILES_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='metrics'
+              isChecked={params.metrics}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.METRICS_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='jmx'
+              isChecked={params.jmx}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.JMX_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='replication'
+              isChecked={params.replication}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.REPLICATION_LABEL}
+          </NxCheckbox>
+        </NxFieldset>
+        <NxFieldset label={UIStrings.SUPPORT_ZIP.OPTIONS}>
+          <NxCheckbox
+              checkboxId='limitFileSizes'
+              isChecked={params.limitFileSizes}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.LIMITFILESIZES_LABEL}
+          </NxCheckbox>
+          <NxCheckbox
+              checkboxId='limitZipSize'
+              isChecked={params.limitZipSize}
+              onChange={setParams}
+          >
+            {UIStrings.SUPPORT_ZIP.LIMITZIPSIZE_LABEL}
+          </NxCheckbox>
+        </NxFieldset>
+      </div>
+    </NxModal.Content>
+
     <footer className="nx-footer">
       <div className="nx-btn-bar">
         {cancel &&
-            <NxButton variant='secondary' onClick={cancel}>
-              <span>{UIStrings.SETTINGS.CANCEL_BUTTON_LABEL}</span>
-            </NxButton>
+        <NxButton variant='secondary' onClick={cancel}>
+          <span>{UIStrings.SETTINGS.CANCEL_BUTTON_LABEL}</span>
+        </NxButton>
         }
 
         <NxButton variant='primary' onClick={submit} type='submit'>
@@ -130,5 +135,5 @@ export default function SupportZipForm({params, setParams, submit, clustered, ha
         }
       </div>
     </footer>
-  </div>
+  </>;
 }
