9dca491d23c6ba51b66ece724dc4a84794157135
Matthew Piggott <mpiggott@sonatype.com>
2022-12-14 09:21:52 -0500

    Public export of Nexus code:   - 2ad9f37ec8a77ffe5de66fd3581e8e07b09faacd by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: 2ad9f37ec8a77ffe5de66fd3581e8e07b09faacd

--- components/nexus-repository-view/src/main/java/org/sonatype/nexus/repository/httpclient/internal/HttpClientFacetImpl.java
+++ components/nexus-repository-view/src/main/java/org/sonatype/nexus/repository/httpclient/internal/HttpClientFacetImpl.java
@@ -21,6 +21,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.Valid;
 
+import org.sonatype.nexus.common.event.EventHelper;
 import org.sonatype.nexus.common.stateguard.Guarded;
 import org.sonatype.nexus.distributed.event.service.api.common.RepositoryRemoteConnectionStatusEvent;
 import org.sonatype.nexus.httpclient.HttpClientManager;
@@ -340,8 +341,15 @@ public class HttpClientFacetImpl
     httpClient = null;
   }
 
-  private void distributeRepositoryConnectionStatusChangedEvent(final String repositoryName,
-                                                                final RemoteConnectionStatus status) {
+  private void distributeRepositoryConnectionStatusChangedEvent(
+      final String repositoryName,
+      final RemoteConnectionStatus status)
+  {
+    if (EventHelper.isReplicating()) {
+      log.debug("Originally triggered by remote event");
+      return;
+    }
+
     log.debug("Distribute repository block changed event: repository={}:{}", repositoryName, status.getType());
 
     long blockUntilMillis = status.getBlockedUntil() == null ? DateTime.now().getMillis()
