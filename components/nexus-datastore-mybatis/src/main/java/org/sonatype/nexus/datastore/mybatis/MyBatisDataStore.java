4a95f8359a0bdc67d56b827939d539f6fba3b637
orudyk <alexrudyk16@gmail.com>
2023-02-10 14:06:39 +0200

    Public export of Nexus code:   - 65b465ac3d72793e7a24a6614e335382bdd8b93a by orudyk <alexrudyk16@gmail.com>
    GitOrigin-RevId: 65b465ac3d72793e7a24a6614e335382bdd8b93a

--- components/nexus-datastore-mybatis/src/main/java/org/sonatype/nexus/datastore/mybatis/MyBatisDataStore.java
+++ components/nexus-datastore-mybatis/src/main/java/org/sonatype/nexus/datastore/mybatis/MyBatisDataStore.java
@@ -51,6 +51,7 @@ import org.sonatype.nexus.crypto.internal.CryptoHelperImpl;
 import org.sonatype.nexus.crypto.internal.MavenCipherImpl;
 import org.sonatype.nexus.datastore.DataStoreSupport;
 import org.sonatype.nexus.datastore.api.DataAccess;
+import org.sonatype.nexus.datastore.api.DataAccessException;
 import org.sonatype.nexus.datastore.api.DataStore;
 import org.sonatype.nexus.datastore.api.Expects;
 import org.sonatype.nexus.datastore.api.SchemaTemplate;
@@ -257,7 +258,16 @@ public class MyBatisDataStore
     info("Creating schema for {}", accessType.getSimpleName());
     try (SqlSession session = new DataAccessSqlSession(mybatisConfig)) {
       DataAccess dao = session.getMapper(accessType);
-      dao.createSchema();
+      boolean done = false;
+      do {
+        try {
+          dao.createSchema();
+          done = true;
+        }
+        catch (DataAccessException dae) {
+          //ignore
+        }
+      } while (!done);
       dao.extendSchema();
       session.commit();
     }
