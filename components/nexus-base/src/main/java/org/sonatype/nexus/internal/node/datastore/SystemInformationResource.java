ffe40c8f5f6011fa4ff648beb0497dfbd9853c49
Sergii Pryhoda <userseprid@gmail.com>
2023-02-27 18:28:57 +0200

    Public export of Nexus code:   - 76646882fcebee7fe7eec4d55736ea95d80d94a3 by Sergii Pryhoda <userseprid@gmail.com>
    GitOrigin-RevId: 76646882fcebee7fe7eec4d55736ea95d80d94a3

---
+++ components/nexus-base/src/main/java/org/sonatype/nexus/internal/node/datastore/SystemInformationResource.java
@@ -0,0 +1,59 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.internal.node.datastore;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.sonatype.goodies.common.ComponentSupport;
+import org.sonatype.nexus.node.datastore.NodeHeartbeatManager;
+import org.sonatype.nexus.rest.Resource;
+
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.sonatype.nexus.rest.APIConstants.BETA_API_PREFIX;
+
+@Produces(APPLICATION_JSON)
+@Path(SystemInformationResource.PATH)
+@Named
+@Singleton
+public class SystemInformationResource extends ComponentSupport
+    implements Resource, SystemInformationResourceDoc
+{
+  public static final String PATH = BETA_API_PREFIX + "/system/information";
+
+  private final NodeHeartbeatManager nodeHeartbeatManager;
+
+  @Inject
+  public SystemInformationResource(final NodeHeartbeatManager nodeHeartbeatManager) {
+    this.nodeHeartbeatManager = checkNotNull(nodeHeartbeatManager);
+  }
+
+  @Override
+  @GET
+  @RequiresAuthentication
+  @RequiresPermissions("nexus:*")
+  public Map<String, Map<String, Object>> getSystemInformation() {
+    return nodeHeartbeatManager.getSystemInformationForNodes();
+  }
+}
---
+++ components/nexus-base/src/main/java/org/sonatype/nexus/internal/node/datastore/SystemInformationResourceDoc.java
@@ -0,0 +1,31 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.internal.node.datastore;
+
+import java.util.Map;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value = "System: Nodes")
+public interface SystemInformationResourceDoc
+{
+  @ApiOperation("Get information about all nodes")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "System information for all running nodes grouped by nodeID"),
+      @ApiResponse(code = 403, message = "Insufficient permissions to request this data")
+  })
+  Map<String, Map<String, Object>> getSystemInformation();
+}
