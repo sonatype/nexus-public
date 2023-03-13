b5daf725420be86b79a74cc7eec965251021db2b
Juan Sebastián Reina <juansereina@outlook.com>
2023-03-13 09:01:15 -0500

    Public export of Nexus code:   - 78108918ab1a4b076f3887b75e55b1668266f725 by Juan Sebastián Reina <juansereina@outlook.com>
    GitOrigin-RevId: 78108918ab1a4b076f3887b75e55b1668266f725

--- plugins/nexus-coreui-plugin/src/frontend/src/components/pages/admin/DataStoreConfiguration/DataStoreConfiguration.jsx
+++ plugins/nexus-coreui-plugin/src/frontend/src/components/pages/admin/DataStoreConfiguration/DataStoreConfiguration.jsx
@@ -1,4 +1,4 @@
- /*
+/*
  * Sonatype Nexus (TM) Open Source Version
  * Copyright (c) 2008-present Sonatype, Inc.
  * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
@@ -19,8 +19,8 @@ import {
   Page,
   PageHeader,
   PageTitle,
-  Section,
   FormUtils,
+  ReadOnlyField,
 } from '@sonatype/nexus-ui-plugin';
 
 import {
@@ -29,6 +29,8 @@ import {
   NxTextInput,
   NxFormGroup,
   NxStatefulForm,
+  NxReadOnly,
+  NxTile,
 } from '@sonatype/react-shared-components';
 
 import DataStoreConfigurationMachine from './DataStoreConfigurationMachine';
@@ -37,60 +39,62 @@ import UIStrings from '../../../../constants/UIStrings';
 const FIELDS = UIStrings.DATASTORE_CONFIGURATION.FIELDS;
 
 export default function DataStoreConfiguration() {
-  const [state, send] = useMachine(DataStoreConfigurationMachine, {devTools: true});
+  const [state, send] = useMachine(DataStoreConfigurationMachine, {
+    devTools: true,
+  });
 
-  const {isPristine} = state.context;
+  const {isPristine, data} = state.context;
   const discard = () => send('RESET');
 
-  return <Page>
-    <PageHeader><PageTitle icon={faServer} {...UIStrings.DATASTORE_CONFIGURATION.MENU}/></PageHeader>
-    <ContentBody className='nxrm-datastore-configuration'>
-      <Section className="user-account-settings">
-        <NxStatefulForm
+  return (
+    <Page>
+      <PageHeader>
+        <PageTitle
+          icon={faServer}
+          {...UIStrings.DATASTORE_CONFIGURATION.MENU}
+        />
+      </PageHeader>
+      <ContentBody className="nxrm-datastore-configuration">
+        <NxTile className="user-account-settings">
+          <NxStatefulForm
             {...FormUtils.formProps(state, send)}
             additionalFooterBtns={
               <NxTooltip title={FormUtils.discardTooltip({isPristine})}>
-                <NxButton type="button" className={isPristine && 'disabled'} onClick={discard}>
+                <NxButton
+                  type="button"
+                  className={isPristine && 'disabled'}
+                  onClick={discard}
+                >
                   {UIStrings.SETTINGS.DISCARD_BUTTON_LABEL}
                 </NxButton>
               </NxTooltip>
             }
-        >
-          <NxFormGroup label={FIELDS.jdbcUrlLabel} isRequired>
-            <NxTextInput
-                {...FormUtils.fieldProps('jdbcUrl', state)}
-                className="nx-text-input--long"
-                disabled
-            />
-          </NxFormGroup>
-          <NxFormGroup label={FIELDS.usernameLabel} isRequired>
-            <NxTextInput
-                {...FormUtils.fieldProps('username', state)}
-                disabled
-            />
-          </NxFormGroup>
-          <NxFormGroup label={FIELDS.schemaLabel} isRequired>
-            <NxTextInput
-                {...FormUtils.fieldProps('schema', state)}
-                disabled
-            />
-          </NxFormGroup>
-          <NxFormGroup label={FIELDS.maxConnectionPoolLabel}>
-            <NxTextInput
+          >
+            <NxReadOnly>
+              <ReadOnlyField label={FIELDS.jdbcUrlLabel} value={data.jdbcUrl} />
+              <ReadOnlyField
+                label={FIELDS.usernameLabel}
+                value={data.username}
+              />
+              <ReadOnlyField label={FIELDS.schemaLabel} value={data.schema} />
+            </NxReadOnly>
+            <NxFormGroup label={FIELDS.maxConnectionPoolLabel}>
+              <NxTextInput
                 {...FormUtils.fieldProps('maximumConnectionPool', state)}
                 onChange={FormUtils.handleUpdate('maximumConnectionPool', send)}
-            />
-          </NxFormGroup>
-          <NxFormGroup label={FIELDS.advancedLabel}>
-            <NxTextInput
+              />
+            </NxFormGroup>
+            <NxFormGroup label={FIELDS.advancedLabel}>
+              <NxTextInput
                 {...FormUtils.fieldProps('advanced', state)}
                 onChange={FormUtils.handleUpdate('advanced', send)}
                 className="nx-text-input--long"
                 type="textarea"
-            />
-          </NxFormGroup>
-        </NxStatefulForm>
-      </Section>
-    </ContentBody>
-  </Page>;
+              />
+            </NxFormGroup>
+          </NxStatefulForm>
+        </NxTile>
+      </ContentBody>
+    </Page>
+  );
 }
