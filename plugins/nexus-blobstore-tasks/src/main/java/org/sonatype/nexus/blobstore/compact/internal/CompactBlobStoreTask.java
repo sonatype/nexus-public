9c6f064a5825082c32f84d3de8b90b8dab8a0cd8
Leonardo Ruiz <36464079+CodingLeoo@users.noreply.github.com>
2023-05-11 11:47:21 -0500

    Public export of Nexus code:   - 0330e3df6bc1498da78c0e049d44db57497aef95 by Leonardo Ruiz <36464079+CodingLeoo@users.noreply.github.com>
    GitOrigin-RevId: 0330e3df6bc1498da78c0e049d44db57497aef95

--- plugins/nexus-blobstore-tasks/src/main/java/org/sonatype/nexus/blobstore/compact/internal/CompactBlobStoreTask.java
+++ plugins/nexus-blobstore-tasks/src/main/java/org/sonatype/nexus/blobstore/compact/internal/CompactBlobStoreTask.java
@@ -12,17 +12,30 @@
  */
 package org.sonatype.nexus.blobstore.compact.internal;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.sonatype.nexus.blobstore.api.BlobStore;
 import org.sonatype.nexus.blobstore.api.BlobStoreManager;
 import org.sonatype.nexus.blobstore.api.BlobStoreUsageChecker;
+import org.sonatype.nexus.repository.move.ChangeRepositoryBlobStoreConfiguration;
+import org.sonatype.nexus.repository.move.ChangeRepositoryBlobStoreStore;
 import org.sonatype.nexus.scheduling.Cancelable;
 import org.sonatype.nexus.scheduling.TaskSupport;
+import org.sonatype.nexus.scheduling.TaskUtils;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static org.sonatype.nexus.blobstore.compact.internal.CompactBlobStoreTaskDescriptor.BLOB_STORE_NAME_FIELD_ID;
+import static org.sonatype.nexus.logging.task.TaskLoggingMarkers.TASK_LOG_ONLY;
 
 /**
  * Task to compact a given blob store.
@@ -36,18 +49,54 @@ public class CompactBlobStoreTask
 {
   private final BlobStoreManager blobStoreManager;
 
+  private final Optional<ChangeRepositoryBlobStoreStore> changeBlobstoreStore;
+
   private final BlobStoreUsageChecker blobStoreUsageChecker;
 
+  private final TaskUtils taskUtils;
+
+
   @Inject
-  public CompactBlobStoreTask(final BlobStoreManager blobStoreManager,
-                              final BlobStoreUsageChecker blobStoreUsageChecker)
+  public CompactBlobStoreTask(
+      final BlobStoreManager blobStoreManager,
+      @Nullable final ChangeRepositoryBlobStoreStore changeBlobstoreStore,
+      final BlobStoreUsageChecker blobStoreUsageChecker,
+      final TaskUtils taskUtils)
   {
     this.blobStoreManager = checkNotNull(blobStoreManager);
+    this.changeBlobstoreStore = Optional.ofNullable(changeBlobstoreStore);
     this.blobStoreUsageChecker = checkNotNull(blobStoreUsageChecker);
+    this.taskUtils = checkNotNull(taskUtils);
+  }
+
+  @VisibleForTesting
+  void checkForConflicts() {
+    String blobStoreName = checkNotNull(getBlobStoreField());
+
+    taskUtils.checkForConflictingTasks(getId(), getName(), asList("repository.move"), ImmutableMap
+        .of("moveInitialBlobstore", asList(blobStoreName), "moveTargetBlobstore", asList(blobStoreName)));
+
+    checkForUnfinishedMoveTask(blobStoreName);
+  }
+
+  private void checkForUnfinishedMoveTask(String blobStoreName) {
+    List<ChangeRepositoryBlobStoreConfiguration> existingMoves = changeBlobstoreStore
+        .map(store -> store.findByBlobStoreName(blobStoreName))
+        .orElseGet(Collections::emptyList);
+
+    if (!existingMoves.isEmpty()) {
+      log.info(TASK_LOG_ONLY, "found {} unfinished move tasks using blobstore '{}', unable to run task '{}'",
+          existingMoves.size(), blobStoreName, getName());
+
+      throw new IllegalStateException(
+          format("found unfinished move task(s) using blobstore '%s', task can't be executed", blobStoreName));
+    }
   }
 
   @Override
   protected Object execute() throws Exception {
+    checkForConflicts();
+
     BlobStore blobStore = blobStoreManager.get(getBlobStoreField());
     if (blobStore != null) {
       blobStore.compact(blobStoreUsageChecker);
