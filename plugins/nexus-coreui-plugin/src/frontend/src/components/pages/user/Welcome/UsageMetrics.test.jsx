a2bd06ba6bf741e0a4290853536465657cd57096
Luis J. Gonzalez <lgonzalez@sonatype.com>
2023-09-21 14:52:12 -0500

    Public export of Nexus code:   - 95ada3e867057a78a651e1433dafc6656ea31ab1 by Luis J. Gonzalez <lgonzalez@sonatype.com>
    GitOrigin-RevId: 95ada3e867057a78a651e1433dafc6656ea31ab1

--- plugins/nexus-coreui-plugin/src/frontend/src/components/pages/user/Welcome/UsageMetrics.test.jsx
+++ plugins/nexus-coreui-plugin/src/frontend/src/components/pages/user/Welcome/UsageMetrics.test.jsx
@@ -18,6 +18,7 @@ import {when} from "jest-when";
 import UsageMetrics from './UsageMetrics';
 import TestUtils from '@sonatype/nexus-ui-plugin/src/frontend/src/interface/TestUtils';
 import {APIConstants, ExtJS} from '@sonatype/nexus-ui-plugin';
+import {act} from "react-dom/test-utils";
 
 const {USAGE_METRICS} = APIConstants.REST.INTERNAL;
 
@@ -30,6 +31,9 @@ jest.mock('@sonatype/nexus-ui-plugin', () => ({
   ...jest.requireActual('@sonatype/nexus-ui-plugin'),
   ExtJS: {
     isProEdition: jest.fn().mockReturnValue(false),
+    state: jest.fn().mockReturnValue({
+      getValue: jest.fn(),
+    }),
   },
 }));
 
@@ -53,6 +57,12 @@ const selectors = {
 };
 
 describe('Usage Metrics', () => {
+  beforeEach( () => {
+    when(ExtJS.state().getValue)
+        .calledWith('nexus.datastore.clustered.enabled')
+        .mockReturnValue(false);
+  });
+
   it('renders data correctly', async () => {
     when(axios.get).calledWith(USAGE_METRICS).mockResolvedValue({
       data: data
@@ -120,5 +130,26 @@ describe('Usage Metrics', () => {
     expect(selectors.getCard('peak requests per day')).toBeInTheDocument();
 
     expect(() => selectors.getCard('unique logins')).toThrow();
+  });
+
+  describe('HA mode', () => {
+    beforeEach( () => {
+      when(ExtJS.state().getValue)
+          .calledWith('nexus.datastore.clustered.enabled')
+          .mockReturnValue(true);
+    });
+
+    it("does not render any card when HA mode on", async() =>{
+
+      await act(async() => {
+        render(<UsageMetrics />);
+      });
+
+      expect(() => selectors.getCard('unique logins')).toThrow();
+      expect(() => selectors.getCard('total components')).toThrow();
+      expect(() => selectors.getCard('peak requests per minute')).toThrow();
+      expect(() => selectors.getCard('peak requests per day')).toThrow();
+    });
+
   });
 })
