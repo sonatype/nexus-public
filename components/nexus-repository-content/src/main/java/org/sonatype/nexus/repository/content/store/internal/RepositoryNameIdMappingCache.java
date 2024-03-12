8438ae29872a999461809a8cb8c57035961529d6
Wilson Herrera <156115536+wilsonhdev@users.noreply.github.com>
2024-03-12 11:08:35 -0500

    Public export of Nexus code:   - f4eef29f64c57110456cd61bcc1cec163c73b7b6 by Wilson Herrera <156115536+wilsonhdev@users.noreply.github.com>
    GitOrigin-RevId: f4eef29f64c57110456cd61bcc1cec163c73b7b6

---
+++ components/nexus-repository-content/src/main/java/org/sonatype/nexus/repository/content/store/internal/RepositoryNameIdMappingCache.java
@@ -0,0 +1,123 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.content.store.internal;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.goodies.common.ComponentSupport;
+import org.sonatype.nexus.common.app.FeatureFlag;
+import org.sonatype.nexus.common.event.EventAware;
+import org.sonatype.nexus.repository.Format;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.content.store.ContentRepositoryStore;
+import org.sonatype.nexus.repository.content.store.FormatStoreManager;
+import org.sonatype.nexus.repository.manager.RepositoryCreatedEvent;
+import org.sonatype.nexus.repository.manager.RepositoryDeletedEvent;
+
+import com.google.common.eventbus.Subscribe;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.common.app.FeatureFlags.REPOSITORY_SIZE_ENABLED;
+import static org.sonatype.nexus.datastore.api.DataStoreManager.DEFAULT_DATASTORE_NAME;
+
+/**
+ * Maintain mapping of repository name and id
+ */
+@Named
+@Singleton
+@FeatureFlag(name = REPOSITORY_SIZE_ENABLED)
+public class RepositoryNameIdMappingCache
+    extends ComponentSupport
+    implements EventAware
+{
+  private volatile Map<String, Integer> nameRepositoryIdMap;
+
+  private final ContentRepositoryStore<?> contentRepositoryStore;
+
+  private final List<String> formatNames;
+
+  @Inject
+  public RepositoryNameIdMappingCache(
+      FormatStoreManager formatStoreManager,
+      final List<Format> formats)
+  {
+    this.contentRepositoryStore = checkNotNull(formatStoreManager).contentRepositoryStore(DEFAULT_DATASTORE_NAME);
+    this.formatNames = checkNotNull(formats).stream().map(Format::getValue).collect(Collectors.toList());
+  }
+
+  @Subscribe
+  public void on(final RepositoryCreatedEvent event) {
+    Repository repository = event.getRepository();
+    log.debug("Handling repository create event for {}", repository.getName());
+    fetchRepositoryId(repository.getName(), repository.getFormat().getValue());
+  }
+
+  @Subscribe
+  public void on(final RepositoryDeletedEvent event) {
+    log.debug("Handling repository deleted event for {}", event.getRepository().getName());
+    if (nameRepositoryIdMap != null) {
+      nameRepositoryIdMap.remove(event.getRepository().getName());
+    }
+  }
+
+  public List<Integer> getRepositoryIds(List<String> repositoryNames, String format) {
+    return repositoryNames.stream()
+        .map(repositoryName -> fetchRepositoryId(repositoryName, format))
+        .filter(OptionalInt::isPresent)
+        .map(OptionalInt::getAsInt)
+        .collect(Collectors.toList());
+  }
+
+  private Map<String, Integer> getCache() {
+    Map<String, Integer> cache = nameRepositoryIdMap;
+    if (cache == null) {
+      synchronized (this) {
+        if (nameRepositoryIdMap == null) {
+          nameRepositoryIdMap = populateCache();
+        }
+        cache = nameRepositoryIdMap;
+      }
+    }
+    return cache;
+  }
+
+  private OptionalInt fetchRepositoryId(String name, String format) {
+    if (getCache().containsKey(name)) {
+      return OptionalInt.of(getCache().get(name));
+    }
+    Optional<Map<String, Object>> repositoryNameId = contentRepositoryStore.readContentRepositoryId(format, name);
+    if (repositoryNameId.isPresent() && repositoryNameId.get().containsValue(name)) {
+      int repositoryId = (Integer) repositoryNameId.get().get("repository_id");
+      nameRepositoryIdMap.put(name, repositoryId);
+      return OptionalInt.of(repositoryId);
+    }
+    else {
+      return OptionalInt.empty();
+    }
+  }
+
+  private Map<String, Integer> populateCache() {
+    Map<String, Integer> cache = new ConcurrentHashMap<>();
+    List<Map<String, Object>> ids = contentRepositoryStore.readAllContentRepositoryIds(this.formatNames);
+    ids.stream().forEach(id -> cache.put((String) id.get("name"), (Integer) id.get("repository_id")));
+    return cache;
+  }
+}
---
+++ components/nexus-repository-content/src/test/java/org/sonatype/nexus/repository/content/store/internal/RepositoryNameIdMappingCacheTest.java
@@ -0,0 +1,116 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.content.store.internal;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.repository.Format;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.content.store.ContentRepositoryStore;
+import org.sonatype.nexus.repository.content.store.FormatStoreManager;
+import org.sonatype.nexus.repository.manager.RepositoryCreatedEvent;
+import org.sonatype.nexus.repository.manager.RepositoryDeletedEvent;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RepositoryNameIdMappingCacheTest
+    extends TestSupport
+{
+  @Mock
+  private FormatStoreManager formatStoreManager;
+
+  @Mock
+  private List<Format> formatNames;
+
+  @Mock
+  private ContentRepositoryStore<?> contentRepositoryStore;
+
+  private RepositoryNameIdMappingCache underTest;
+
+  @Before
+  public void setup() {
+    when(formatStoreManager.contentRepositoryStore(any())).thenReturn(contentRepositoryStore);
+    underTest = new RepositoryNameIdMappingCache(formatStoreManager, formatNames);
+  }
+
+  @Test
+  public void testGetRepositoryIds() {
+    List<Integer> repositoryIds = initializeCache();
+    assertThat(repositoryIds, contains(1, 2));
+  }
+
+  @Test
+  public void testShouldAddNewRepositoryIdOnRepositoryCreatedEvent() {
+    initializeCache();
+    Map<String, Object> nameIdThree = getRepositoryNameId("repo3", 3);
+
+    when(contentRepositoryStore.readContentRepositoryId(any(), any())).thenReturn(Optional.of(nameIdThree));
+
+    underTest.on(new RepositoryCreatedEvent(repository("repo3")));
+
+    List<Integer> repositoryIds = underTest.getRepositoryIds(Arrays.asList("repo3"), "raw");
+    assertThat(repositoryIds, contains(3));
+  }
+
+  @Test
+  public void testShouldRemoveRepositoryIdOnRepositoryDeletedEvent() {
+    initializeCache();
+
+    underTest.on(new RepositoryDeletedEvent(repository("repo1")));
+
+    List<Integer> repositoryIds = underTest.getRepositoryIds(Arrays.asList("repo1"), "raw");
+    assertThat(repositoryIds, not(hasItem(1)));
+  }
+
+  private List<Integer> initializeCache() {
+    List<String> repositoryNames = Arrays.asList("repo1", "repo2");
+    Map<String, Object> nameIdOne = getRepositoryNameId("repo1", 1);
+    Map<String, Object> nameIdTwo = getRepositoryNameId("repo2", 2);
+    List<Map<String, Object>> repositoryNameIds = Arrays.asList(nameIdOne, nameIdTwo);
+
+    when(contentRepositoryStore.readAllContentRepositoryIds(any())).thenReturn(repositoryNameIds);
+
+    return underTest.getRepositoryIds(repositoryNames, "raw");
+  }
+
+  private Map<String, Object> getRepositoryNameId(String name, int id) {
+    Map<String, Object> nameId = new HashMap<>();
+    nameId.put("name", name);
+    nameId.put("repository_id", id);
+    return nameId;
+  }
+
+  private Repository repository(String name) {
+    Repository repository = mock(Repository.class);
+    when(repository.getName()).thenReturn(name);
+    when(repository.getName()).thenReturn(name);
+    when(repository.getFormat()).thenReturn(mock(Format.class));
+    return repository;
+  }
+}
