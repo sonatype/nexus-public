5e726dfa33ce9de3d697daae6723bee15b046c5b
logresearch <log.researchovo@gmail.com>
2024-07-03 16:14:11 +0800

    Fix logging statement

--- testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/TaskTestSystem.java
+++ testsuite/nexus-repository-testsupport/src/main/java/org/sonatype/nexus/testsuite/testsupport/system/TaskTestSystem.java
@@ -70,7 +70,7 @@ public class TaskTestSystem
 
   @Subscribe
   public void on(final TaskEvent event) {
-    log.debug("Recieved event: {}", event);
+    log.debug("Received event: {}", event);
     events.add(event);
   }
 
