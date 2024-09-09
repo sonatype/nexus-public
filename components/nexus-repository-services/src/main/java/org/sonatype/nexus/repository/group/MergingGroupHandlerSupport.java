51361cbf79a5f0ad43c157706151fe504b649f0c
Eugene Bulatnikov <ybulatnikov@sonatype.com>
2024-09-09 14:27:55 +0300

    Public export of Nexus code:   - 115a5dec32eb2fc89f1fde315308c3070ffb4d0e by Eugene Bulatnikov <ybulatnikov@sonatype.com>
    GitOrigin-RevId: 115a5dec32eb2fc89f1fde315308c3070ffb4d0e

--- components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/group/MergingGroupHandlerSupport.java
+++ components/nexus-repository-services/src/main/java/org/sonatype/nexus/repository/group/MergingGroupHandlerSupport.java
@@ -24,7 +24,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 import javax.annotation.Nonnull;
 
 import org.sonatype.nexus.common.collect.AttributesMap;
@@ -115,11 +114,6 @@ public abstract class MergingGroupHandlerSupport
       headers.set(HttpHeaders.RANGE, rangeheader);
     }
 
-    if (successfulResponses.isEmpty()) {
-      return getCached(context)
-          .map(HttpResponses::ok)
-          .orElse(notFoundResponse(context));
-    }
     if (successfulResponses.size() == 1 && shouldReturnOnlyRespondingMember(context.getRepository())) {
       // When only a single member responds successfully we return its value and don't bother to persist.
       return Iterables.getOnlyElement(successfulResponses.values());
