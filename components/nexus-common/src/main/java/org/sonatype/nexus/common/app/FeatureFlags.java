37ceeea7a985a5e102d3eaa819f28470e502a196
Julian Bermudez Valderrama <jbermudez@sonatype.com>
2024-09-19 03:42:34 -0500

    Public export of Nexus code:   - 461304e7628c6e076e55eeeec3901555c03d0f0b by Julian Bermudez Valderrama <jbermudez@sonatype.com>
    GitOrigin-RevId: 461304e7628c6e076e55eeeec3901555c03d0f0b

--- components/nexus-common/src/main/java/org/sonatype/nexus/common/app/FeatureFlags.java
+++ components/nexus-common/src/main/java/org/sonatype/nexus/common/app/FeatureFlags.java
@@ -185,11 +185,11 @@ public interface FeatureFlags
 
   String MALICIOUS_RISK_ENABLED = "nexus.malicious.risk.enabled";
 
-  String MALICIOUS_RISK_ENABLED_NAMED = "${nexus.malicious.risk.enabled:-false}";
+  String MALICIOUS_RISK_ENABLED_NAMED = "${nexus.malicious.risk.enabled:-true}";
 
   String MALICIOUS_RISK_ON_DISK_ENABLED = "nexus.malicious.risk.on.disk.enabled";
 
-  String MALICIOUS_RISK_ON_DISK_ENABLED_NAMED = "${nexus.malicious.risk.on.disk.enabled:-false}";
+  String MALICIOUS_RISK_ON_DISK_ENABLED_NAMED = "${nexus.malicious.risk.on.disk.enabled:-true}";
 
   String CONAN_V2_ENABLED = "nexus.conan.v2.enabled";
 
