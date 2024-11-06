17c812d72524915cf652e2658f5da9bd983d05e1
Matthew Piggott <mpiggott@sonatype.com>
2024-11-06 16:00:59 -0500

    Public export of Nexus code:   - 84ba8d368f70a4df155a3c2ccf72e64a60317181 by Matthew Piggott <mpiggott@sonatype.com>
    GitOrigin-RevId: 84ba8d368f70a4df155a3c2ccf72e64a60317181

--- components/nexus-common/src/main/java/org/sonatype/nexus/common/json/JsonMapperProvider.java
+++ components/nexus-common/src/main/java/org/sonatype/nexus/common/json/JsonMapperProvider.java
@@ -19,6 +19,7 @@ import javax.inject.Provider;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -43,6 +44,7 @@ public class JsonMapperProvider
         .addModule(new JavaTimeModule())
         .addModule(new Jdk8Module())
         .addModule(new ParameterNamesModule())
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .build();
   }
 }
