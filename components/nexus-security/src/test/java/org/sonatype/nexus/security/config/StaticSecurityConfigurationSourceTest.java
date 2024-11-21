e5480698408dbb39c110cfb18a2d6dee3d9635bb
Damian Bradicich <dbradicich@sonatype.com>
2024-11-21 10:51:34 -0500

    Public export of Nexus code:   - cb001ab6204a6773adff2b117181c4c8187a0171 by Damian Bradicich <dbradicich@sonatype.com>
    GitOrigin-RevId: cb001ab6204a6773adff2b117181c4c8187a0171

--- components/nexus-security/src/test/java/org/sonatype/nexus/security/config/StaticSecurityConfigurationSourceTest.java
+++ components/nexus-security/src/test/java/org/sonatype/nexus/security/config/StaticSecurityConfigurationSourceTest.java
@@ -14,28 +14,26 @@ package org.sonatype.nexus.security.config;
 
 import java.io.IOException;
 
-import org.sonatype.goodies.testsupport.TestSupport;
+import org.sonatype.nexus.security.AbstractSecurityTest;
 
 import org.apache.shiro.authc.credential.PasswordService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class StaticSecurityConfigurationSourceTest
-    extends TestSupport
+    extends AbstractSecurityTest
 {
-  @Mock
-  private PasswordService passwordService;
+  private PasswordService passwordService = mock(PasswordService.class);
 
-  @Mock
-  private AdminPasswordFileManager adminPasswordFileManager;
+  private AdminPasswordFileManager adminPasswordFileManager = mock(AdminPasswordFileManager.class);
 
   private StaticSecurityConfigurationSource underTest;
 
