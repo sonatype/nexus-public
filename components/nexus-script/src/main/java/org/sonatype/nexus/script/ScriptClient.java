af5bdab31d2f535ba66a51c6dde2049267318565
Jacob Arcila Cardenas <156114763+jarcilac@users.noreply.github.com>
2024-11-26 17:49:53 -0500

    Public export of Nexus code:   - 7f16a5804c533b8fee103c5612db542c31bc41a7 by Jacob Arcila Cardenas <156114763+jarcilac@users.noreply.github.com>
    GitOrigin-RevId: 7f16a5804c533b8fee103c5612db542c31bc41a7

--- components/nexus-script/src/main/java/org/sonatype/nexus/script/ScriptClient.java
+++ components/nexus-script/src/main/java/org/sonatype/nexus/script/ScriptClient.java
@@ -10,77 +10,70 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.script
+package org.sonatype.nexus.script;
 
-import javax.validation.Valid
-import javax.validation.constraints.NotNull
-import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.PUT
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
-
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 /**
  * Public API for managing Scripts. Provides BREAD capabilities.
- * 
+ *
  * @since 3.0
  */
 @Path(ScriptClient.RESOURCE_URI)
-@Produces([APPLICATION_JSON])
-@Consumes([APPLICATION_JSON])
-interface ScriptClient
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface ScriptClient
 {
-  public static final String RESOURCE_URI = '/v1/script'
-  
-  public static final String RUN_ACTION = 'run'
+
+  String RESOURCE_URI = "/v1/script";
+
+  String RUN_ACTION = "run";
 
   /**
    * Browse all {@link Script}.
    */
   @GET
-  List<ScriptXO> browse()
+  List<ScriptXO> browse();
 
   /**
    * Get a specific {@link Script} by name.
    */
   @GET
-  @Path('{name}')
-  ScriptXO read(@PathParam('name') String name)
+  @Path("{name}")
+  ScriptXO read(@PathParam("name") String name);
 
   /**
    * Edit an existing {@link Script}.
    */
   @PUT
-  @Path('{name}')
-  void edit(@PathParam('name') String name, @NotNull @Valid ScriptXO scriptXO)
+  @Path("{name}")
+  void edit(@PathParam("name") String name, @NotNull @Valid ScriptXO scriptXO);
 
   /**
    * Add a new {@link Script}.
    */
   @POST
-  void add(@NotNull @Valid ScriptXO scriptXO)
+  void add(@NotNull @Valid ScriptXO scriptXO);
 
   /**
    * Delete an existing {@link Script}.
    */
   @DELETE
-  @Path('{name}')
-  void delete(@PathParam('name') String name)
+  @Path("{name}")
+  void delete(@PathParam("name") String name);
 
   /**
    * Run an existing {@link Script}.
+   * 
    * @param name the name of the Script to execute
    * @param args the arguments to pass to the Script
    */
   @POST
-  @Path('{name}/run')
-  @Consumes([TEXT_PLAIN, APPLICATION_JSON])
-  ScriptResultXO run(@PathParam('name') String name, String args)
-
+  @Path("{name}/run")
+  @Consumes({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})
+  ScriptResultXO run(@PathParam("name") String name, String args);
 }
