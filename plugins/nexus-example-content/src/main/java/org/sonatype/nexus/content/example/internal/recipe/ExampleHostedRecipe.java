03f1961fa37742fbeb1c0289d6b583d3fa53f292
Olu Shiyanbade <oshiyanbade@sonatype.com>
2024-12-05 11:30:54 +0000

    Public export of Nexus code:   - 36cc46ee324c35e39dcf19314b5791df8c93b602 by Olu Shiyanbade <oshiyanbade@sonatype.com>
    GitOrigin-RevId: 36cc46ee324c35e39dcf19314b5791df8c93b602

--- plugins/nexus-example-content/src/main/java/org/sonatype/nexus/content/example/internal/recipe/ExampleHostedRecipe.java
+++ plugins/nexus-example-content/src/main/java/org/sonatype/nexus/content/example/internal/recipe/ExampleHostedRecipe.java
@@ -10,28 +10,28 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.content.example.internal.recipe
+package org.sonatype.nexus.content.example.internal.recipe;
 
-import javax.annotation.Nonnull
-import javax.inject.Inject
-import javax.inject.Named
-import javax.inject.Singleton
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
-import org.sonatype.nexus.repository.Format
-import org.sonatype.nexus.repository.Repository
-import org.sonatype.nexus.repository.Type
-import org.sonatype.nexus.repository.http.HttpHandlers
-import org.sonatype.nexus.repository.http.HttpMethods
-import org.sonatype.nexus.repository.types.HostedType
-import org.sonatype.nexus.repository.view.ConfigurableViewFacet
-import org.sonatype.nexus.repository.view.Route
-import org.sonatype.nexus.repository.view.Router
-import org.sonatype.nexus.repository.view.ViewFacet
-import org.sonatype.nexus.repository.view.matchers.ActionMatcher
-import org.sonatype.nexus.repository.view.matchers.SuffixMatcher
-import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher
+import org.sonatype.nexus.repository.Format;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.Type;
+import org.sonatype.nexus.repository.http.HttpHandlers;
+import org.sonatype.nexus.repository.http.HttpMethods;
+import org.sonatype.nexus.repository.types.HostedType;
+import org.sonatype.nexus.repository.view.ConfigurableViewFacet;
+import org.sonatype.nexus.repository.view.Route;
+import org.sonatype.nexus.repository.view.Router;
+import org.sonatype.nexus.repository.view.ViewFacet;
+import org.sonatype.nexus.repository.view.matchers.ActionMatcher;
+import org.sonatype.nexus.repository.view.matchers.SuffixMatcher;
+import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher;
 
-import static org.sonatype.nexus.repository.view.matchers.logic.LogicMatchers.and
+import static org.sonatype.nexus.repository.view.matchers.logic.LogicMatchers.and;
 
 /**
  * Example hosted recipe.
@@ -40,43 +40,43 @@ import static org.sonatype.nexus.repository.view.matchers.logic.LogicMatchers.an
  */
 @Named(ExampleHostedRecipe.NAME)
 @Singleton
-class ExampleHostedRecipe
+public class ExampleHostedRecipe
     extends ExampleRecipeSupport
 {
-  public static final String NAME = 'example-hosted'
+  public static final String NAME = "example-hosted";
 
   @Inject
-  ExampleHostedRecipe(@Named(HostedType.NAME) final Type type,
-                      @Named(ExampleFormat.NAME) final Format format)
+  public ExampleHostedRecipe(
+      @Named(HostedType.NAME) final Type type,
+      @Named(ExampleFormat.NAME) final Format format)
   {
-    super(type, format)
+    super(type, format);
   }
 
   @Override
-  void apply(@Nonnull final Repository repository) throws Exception {
-    repository.attach(securityFacet.get())
-    repository.attach(configure(viewFacet.get()))
-    repository.attach(contentFacet.get())
+  public void apply(@Nonnull final Repository repository) throws Exception {
+    repository.attach(securityFacet.get());
+    repository.attach(configure(viewFacet.get()));
+    repository.attach(contentFacet.get());
   }
 
   /**
    * Configure {@link ViewFacet}.
    */
   private ViewFacet configure(final ConfigurableViewFacet facet) {
-    Router.Builder builder = new Router.Builder()
+    Router.Builder builder = new Router.Builder();
 
     // Additional handlers, such as the lastDownloadHandler, are intentionally
     // not included on this route because this route forwards to the route below.
     // This route specifically handles GET / and forwards to /index.html.
     builder.route(new Route.Builder()
-        .matcher(and(new ActionMatcher(HttpMethods.GET), new SuffixMatcher('/')))
+        .matcher(and(new ActionMatcher(HttpMethods.GET), new SuffixMatcher("/")))
         .handler(timingHandler)
         .handler(indexHtmlForwardHandler)
-        .create()
-    )
+        .create());
 
     builder.route(new Route.Builder()
-        .matcher(new TokenMatcher('{path:/.+}'))
+        .matcher(new TokenMatcher("{path:/.+}"))
         .handler(timingHandler)
         .handler(securityHandler)
         .handler(exceptionHandler)
@@ -86,12 +86,12 @@ class ExampleHostedRecipe
         .handler(contentHeadersHandler)
         .handler(lastDownloadedHandler)
         .handler(contentHandler)
-        .create())
+        .create());
 
-    builder.defaultHandlers(HttpHandlers.badRequest())
+    builder.defaultHandlers(HttpHandlers.badRequest());
 
-    facet.configure(builder.create())
+    facet.configure(builder.create());
 
-    return facet
+    return facet;
   }
 }
