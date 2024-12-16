f85b42b8093ecebc5f0c0df60bbd647928620029
Eugene Bulatnikov <ybulatnikov@sonatype.com>
2024-12-16 16:11:46 +0200

    Public export of Nexus code:   - 5b643deb628d17fe9c8c9ff8f84ee20d34e5c59b by Eugene Bulatnikov <ybulatnikov@sonatype.com>
    GitOrigin-RevId: 5b643deb628d17fe9c8c9ff8f84ee20d34e5c59b

--- components/nexus-bootstrap/src/test/java/org/sonatype/nexus/bootstrap/jetty/NexusRequestCustomizerTest.java
+++ components/nexus-bootstrap/src/test/java/org/sonatype/nexus/bootstrap/jetty/NexusRequestCustomizerTest.java
@@ -14,24 +14,38 @@ package org.sonatype.nexus.bootstrap.jetty;
 
 import org.sonatype.goodies.testsupport.TestSupport;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.http.MetaData;
+import org.eclipse.jetty.server.HttpChannel;
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpInput;
+import org.eclipse.jetty.server.HttpOutput;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.server.ServerConnector;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import static org.eclipse.jetty.http.HttpVersion.HTTP_1_1;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-public class DockerSubdomainRequestCustomizerTest
+@RunWith(JUnitParamsRunner.class)
+public class NexusRequestCustomizerTest
     extends TestSupport
 {
-  private DockerSubdomainRequestCustomizer underTest;
+  private NexusRequestCustomizer underTest;
 
   @Mock
   private ServerConnector connector;
@@ -46,8 +60,8 @@ public class DockerSubdomainRequestCustomizerTest
   private Request request;
 
   @Before
-  public void setUp() throws Exception {
-    underTest = new DockerSubdomainRequestCustomizer("nexus", 8081, 8443);
+  public void setUp() {
+    underTest = new NexusRequestCustomizer("", 8081, 8443);
     when(request.getHeader("Host")).thenReturn("test");
     when(request.getHttpURI()).thenReturn(httpURI);
   }
@@ -92,5 +106,50 @@ public class DockerSubdomainRequestCustomizerTest
     underTest.customize(connector, httpConfig, request);
 
     verify(request, times(2)).getHeader("Host");
+  }
+
+  @Test
+  @Parameters({
+      // docker subdomain requests
+      "/v2/, , /repository/docker-repo/v2/",
+      "/v2/token, , /repository/docker-repo/v2/token",
+      "/v2/, /context, /context/repository/docker-repo/v2/",
+      "/v2/token, /context, /context/repository/docker-repo/v2/token",
+      // docker behind reverse proxy token requests
+      "/repository/docker-repo/repository/docker-repo/v2/token, , /repository/docker-repo/v2/token",
+      "/context/repository/docker-repo/context/repository/docker-repo/v2/token, /context, /context/repository/docker-repo/v2/token"
+  })
+  public void testRequestCustomization(
+      final String incomingRequestPath,
+      final String contextPath,
+      final String expectedPath)
+  {
+    underTest = new NexusRequestCustomizer(contextPath, 8081, 8443);
+    Request request1 = prepareRequest(incomingRequestPath);
+
+    underTest.customize(connector, httpConfig, request1);
+
+    String actualPath = request1.getHttpURI().getPath();
+
+    assertThat(actualPath, is(expectedPath));
+  }
+
+  private Request prepareRequest(String currentPath) {
+    HttpChannel httpChannel = mock(HttpChannel.class);
+    when(httpChannel.getResponse()).thenReturn(mock(Response.class));
+    when(httpChannel.getResponse().getHttpOutput()).thenReturn(mock(HttpOutput.class));
+
+    when(connector.getLocalPort()).thenReturn(8081);
+
+    Request request1 = new Request(httpChannel, mock(HttpInput.class));
+    HttpURI currentUri = new HttpURI("http://localhost:8081" + currentPath);
+    HttpFields headers = new HttpFields();
+    headers.put("Host", "test.localhost");
+    MetaData.Request currentMetadata = new MetaData.Request("GET", currentUri, HTTP_1_1, headers);
+    request1.setMetaData(currentMetadata);
+
+    DockerSubdomainRepositoryMapping.put("test", "docker-repo");
+
+    return request1;
   }
 }
